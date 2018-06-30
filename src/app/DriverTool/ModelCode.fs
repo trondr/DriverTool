20d31ed723be0c33a850a5141dd9d9ac02036244
trondr <trondr@outlook.com>
2018-07-01 00:05:18 +0200

    Add ModelCode class

---
+++ src/app/DriverTool/ModelCode.fs
@@ -0,0 +1,34 @@
+﻿namespace DriverTool
+open System
+
+type ModelCode private (modelCode : string) = 
+    member x.Value = modelCode
+    
+    static member createWithContinuation success failure (modelCode:string) (defaultToLocal:bool) : Result<'ModelCode, 'Exception> =
+        let getModelCodeForLocalSystem : Result<'string, 'Exception> = 
+            let localModelCodeResult = WmiHelper.getWmiProperty "Win32_ComputerSystem" "Model"
+            localModelCodeResult
+        
+        match modelCode with
+        | modelCode when System.String.IsNullOrWhiteSpace(modelCode) && defaultToLocal -> 
+            match getModelCodeForLocalSystem with
+            | Ok mc -> success (ModelCode (mc.ToString()) )
+            | Error ex -> failure (new Exception(sprintf "%s" ex.Message))
+            
+        | modelCode when System.String.IsNullOrWhiteSpace(modelCode) -> failure (new ArgumentNullException("Model code value cannot be null") :> Exception)
+        | _ -> success (ModelCode modelCode)
+
+    static member create (modelCode : string) =
+        let success (value : ModelCode) = Result.Ok value
+        let failure ex = Result.Error ex
+        ModelCode.createWithContinuation success failure modelCode
+    
+    override x.GetHashCode() =
+        hash (modelCode)
+    
+    override x.Equals(b) =
+        match b with
+        | :? ModelCode as m -> (modelCode) = (m.Value)
+        | _ -> false
+
+
---
+++ src/test/DriverTool.Tests/ModelCodeTests.fs
@@ -0,0 +1,30 @@
+﻿namespace DriverTool.Tests
+open NUnit.Framework
+open DriverTool
+open System
+
+[<TestFixture>]
+module ModelCodeTests  =
+    [<Test>]
+    let ModuleCodeTest() =
+        let modelCodeResult = ModelCode.create "EQ10" false
+        match modelCodeResult with
+        |Ok modelCode -> Assert.IsFalse((String.IsNullOrWhiteSpace(modelCode.Value)), sprintf "Model code: %s" modelCode.Value)
+        |Error ex -> Assert.Fail(sprintf "Did not expect to fail. Error: %s" ex.Message)
+
+    [<Test>]
+    let ModuleCodeTest_Empty_ModuleCode_UseLocalIsFalse() =
+        let modelCodeResult = ModelCode.create "" false
+        match modelCodeResult with
+        |Ok modelCode -> Assert.IsFalse((String.IsNullOrWhiteSpace(modelCode.Value)), sprintf "Model code: %s" modelCode.Value)
+        |Error ex -> Assert.IsTrue(ex.Message.StartsWith("Value cannot be null."))
+
+    [<Test>]
+    let ModuleCodeTest_Empty_ModuleCode_UseLocalIsTrue() =
+        let modelCodeResult = ModelCode.create "" true
+        match modelCodeResult with
+        |Ok modelCode -> Assert.IsFalse((String.IsNullOrWhiteSpace(modelCode.Value)), sprintf "Model code: %s" modelCode.Value)
+        |Error ex -> Assert.Fail(ex.Message)
+
+
+
