d5877a136dab810e8520532b2070236efbba55b8
trondr <trondr@outlook.com>
2018-06-30 22:39:04 +0200

    Add WmiHelper

---
+++ src/app/DriverTool/WmiHelper.fs
@@ -0,0 +1,24 @@
+﻿
+
+module WmiHelper
+
+    open System.Management
+    open Microsoft.FSharp.Collections    
+    open System
+
+    let getWmiProperty (className : string) (propertyName : string) : Result<'T, 'Exception> = 
+        try
+            use managementClass = new ManagementClass(className)
+            use managementObjectCollection = managementClass.GetInstances()
+            if(managementObjectCollection.Count = 0) then                
+                Result.Error (new System.Exception(String.Format("No instances of wmi class '{0}' was found", className)))
+            else
+                let value = 
+                    managementObjectCollection
+                    |> Seq.cast
+                    |> Seq.map(fun (x: ManagementObject) -> x.GetPropertyValue(propertyName))
+                    |> Seq.head
+                Result.Ok (value : 'T)
+        with
+           | _ as ex -> Result.Error ex
+
---
+++ src/test/DriverTool.Tests/WmiHelperTests.fs
@@ -0,0 +1,29 @@
+﻿module WmiHelperTests
+open NUnit.Framework
+open System
+
+[<TestFixture>]
+module WmiHelperTests  =
+    [<Test>]
+    let WmiHelperTest_Success() =
+        let actualResult = WmiHelper.getWmiProperty "Win32_ComputerSystem" "Name"
+        let expected = System.Environment.GetEnvironmentVariable("COMPUTERNAME")
+        match actualResult with
+        |Ok actual -> Assert.AreEqual(expected,actual,"Computer name not expected.")
+        |Error e -> Assert.Fail(String.Format("{0}", e.Message))
+        
+    [<Test>]
+    let WmiHelperTest_InvalidClassName() =
+        let actualResult = WmiHelper.getWmiProperty "Win32_ComputerSystem2" "Name"        
+        match actualResult with
+        |Ok actual -> Assert.Fail("This test did not fail as expected due to invalid class name.")
+        |Error e -> Assert.AreEqual("Not found ",String.Format("{0}", e.Message))
+    
+    [<Test>]
+    let WmiHelperTest_InvalidPropertyName() =
+        let actualResult = WmiHelper.getWmiProperty "Win32_ComputerSystem" "Name2"
+        let expected = System.Environment.GetEnvironmentVariable("COMPUTERNAME")
+        match actualResult with
+        |Ok actual -> Assert.Fail("This test did not fail as expected due to invalid property name.")
+        |Error e -> Assert.AreEqual("Not found ",String.Format("{0}", e.Message))
+
