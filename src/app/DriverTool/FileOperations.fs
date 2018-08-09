a8be0af87aa3cef781685a21595da09093ccdb3d
trondr <trondr@outlook.com>
2018-08-09 10:54:42 +0200

    Implement ensureFileDoesNotExist function

---
+++ src/app/DriverTool/FileOperations.fs
@@ -0,0 +1,25 @@
+﻿module FileOperations
+
+open F
+open DriverTool
+open System
+
+let deleteFileUnsafe (path: Path)  =
+    System.IO.File.Delete path.Value
+
+let deleteFile path = 
+    let deleteFileResult = tryCatch deleteFileUnsafe path
+    match deleteFileResult with
+    | Ok _ -> Result.Ok path
+    | Error ex -> Result.Error ex
+
+type FileExistsException(message : string) =
+    inherit Exception(message)    
+
+let ensureFileDoesNotExist (path:Path) overwrite : Result<Path, Exception> = 
+    match System.IO.File.Exists(path.Value) with
+    | true -> 
+        match overwrite with
+        | true -> deleteFile path        
+        | false -> Result.Error (new FileExistsException(String.Format("File exists: '{0}'", path.Value)) :> Exception)
+    | false -> Result.Ok path
---
+++ src/test/DriverTool.Tests/FileOperationTests.fs
@@ -0,0 +1,50 @@
+﻿namespace DriverTool.Tests
+open System
+open NUnit.Framework
+open FileOperations
+
+[<TestFixture>]
+module FileOperationTests =
+    open DriverTool
+
+    type TemporaryFile() =
+        let createTestFile =                        
+            match Path.create (System.IO.Path.GetTempFileName()) with
+            | Ok path -> path
+            | Error ex -> raise ex
+        
+        member _this.Path = createTestFile
+        interface IDisposable with
+            member this.Dispose() =
+                match System.IO.File.Exists(this.Path.Value) with
+                | true -> System.IO.File.Delete(this.Path.Value)
+                | false -> ()
+        
+
+    [<Test>]
+    let ensureFileDoesNotExistTest_FileExists() =
+        use temporaryFile = new TemporaryFile()
+        let path = temporaryFile.Path
+        let actualResult = ensureFileDoesNotExist path false
+        match actualResult with
+        |Ok p -> Assert.Fail((sprintf "The test should have failed. File %s exists" p.Value))
+        |Error ex -> Assert.AreEqual(String.Format("File exists: '{0}'", path.Value),ex.Message)
+
+    [<Test>]
+    let ensureFileDoesNotExistTest_FileExists_Overwrite() =
+        use temporaryFile = new TemporaryFile()
+        let path = temporaryFile.Path
+        let actualResult = ensureFileDoesNotExist path true
+        match actualResult with
+        |Ok p -> Assert.AreEqual(path.Value, p.Value)
+        |Error ex -> Assert.Fail("Test failed")
+
+    [<Test>]
+    let ensureFileDoesNotExistTest_DoesNotExists() =
+        use temporaryFile = new TemporaryFile()
+        let path = temporaryFile.Path
+        System.IO.File.Delete(path.Value)
+        let actualResult = ensureFileDoesNotExist path false
+        match actualResult with
+        |Ok p -> Assert.AreEqual(path.Value, p.Value)
+        |Error ex -> Assert.Fail("Test failed")
