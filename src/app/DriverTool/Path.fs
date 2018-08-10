19ddbc1b604f47d30480f5dc769d0edf65c987ef
trondr <trondr@outlook.com>
2018-08-10 12:02:33 +0200

    Improve Path validation

--- src/app/DriverTool/Path.fs
+++ src/app/DriverTool/Path.fs
@@ -3,13 +3,20 @@
 open System    
 open Paths
 
+type InvalidPathException(path:string, message : string) =
+        inherit Exception(
+            match String.IsNullOrWhiteSpace(message) with
+            |false  -> String.Format("The path '{0}' is not valid. {1}", path, message)
+            |true -> String.Format("The path '{0}' is not valid.", path)
+            )
+
 type Path private (path : string) = 
     member x.Value = path
-    static member createWithContinuation success failure (path:string) : Result<'Path, 'Exception> =
+    static member createWithContinuation success failure (path:string) : Result<Path, Exception> =
         match path with
-        | null -> failure (new ArgumentNullException("Path value cannot be null") :> Exception)
-        | "" -> failure (new ArgumentException("Path value cannot be empty") :> Exception)
-        | path when not (IsValidPath path)  -> failure (new ArgumentException(System.String.Format ("The path '{0}' is not a valid path", path)) :> Exception)
+        | null -> failure (new InvalidPathException("","Path cannot be null.") :> Exception)
+        | "" -> failure (new InvalidPathException("","Path cannot be empty.") :> Exception)
+        | path when not (IsValidPath path)  -> failure ((new InvalidPathException(path,"")) :> Exception)
         | _ -> success (Path (System.IO.Path.GetFullPath(path)))
     
     static member create (path : string) =
