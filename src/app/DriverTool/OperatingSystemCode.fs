0ff591bb7e276e98499ccce4679756f4e41aa0f3
trondr <trondr@outlook.com>
2018-08-10 12:00:45 +0200

    Fixed OperatingSystemCode type validation

--- src/app/DriverTool/OperatingSystemCode.fs
+++ src/app/DriverTool/OperatingSystemCode.fs
@@ -1,37 +1,46 @@
 ﻿namespace DriverTool
 open System
 
+type InvalidOperatingSystemCodeException(operatingSytemCode:string, message : string) =
+        inherit Exception(
+            match String.IsNullOrWhiteSpace(message) with
+            |false  -> String.Format("The operating system code '{0}' is not valid. Valid values are: Win7, Win8, Win10. {1}", operatingSytemCode, message)
+            |true -> String.Format("The operating system code '{0}' is not valid. Valid values are: Win7, Win8, Win10.", operatingSytemCode)
+            )
+
 type OperatingSystemCode private (operatingSystemCode : string) = 
     member x.Value = operatingSystemCode
     
-    static member createWithContinuation success failure (operatingSystemCode:string) (defaultToLocal:bool) : Result<OperatingSystemCode, Exception> =
-        
+    static member createWithContinuation success failure (operatingSystemCode:string) (defaultToLocal:bool) : Result<OperatingSystemCode, Exception> =        
         let OsCaptionToOperatingSystemCode (caption:string) = 
             match caption with
             | caption when caption.Contains("Windows 10") -> Result.Ok "Win10"
             | caption when caption.Contains("Windows 7") -> Result.Ok "Win7"
             | caption when caption.Contains("Windows 8") -> Result.Ok "Win8"
-            | _ -> Result.Error (new Exception(sprintf "OS %s is not supported." caption))
+            | _ -> Result.Error ((new InvalidOperatingSystemCodeException(String.Empty,String.Format("Operating System '{0}' is not supported.", caption))) :> Exception)
 
         let getOperatingSystemCodeForLocalSystem : Result<string, Exception> = 
             let captionResult = WmiHelper.getWmiProperty "Win32_OperatingSystem" "Caption"
             match captionResult with
             | Ok c -> OsCaptionToOperatingSystemCode (c.ToString())
-            | Error ex -> Result.Error ex
+            | Error ex -> Result.Error ((new InvalidOperatingSystemCodeException(String.Empty,String.Format("Failed to get operating system from WMI. {0}", ex.Message))) :> Exception)
         
-        match operatingSystemCode with
-        | operatingSystemCode when System.String.IsNullOrWhiteSpace(operatingSystemCode) && defaultToLocal -> 
-            match getOperatingSystemCodeForLocalSystem with
-            | Ok s -> 
-                match s.ToString().ToUpper() with
+        let validateOperatingSystemCode (operatingSystemCode:string) =
+            match operatingSystemCode with
+            | operatingSystemCode when System.String.IsNullOrWhiteSpace(operatingSystemCode) -> failure (new InvalidOperatingSystemCodeException(operatingSystemCode,"OperatingSystemCode cannot be null or empty.") :> Exception)
+            | _ ->
+                match operatingSystemCode.ToUpper() with
                 | "WIN7" -> success (OperatingSystemCode ("Win7") )
                 | "WIN8" -> success (OperatingSystemCode ("Win8") )
                 | "WIN10" -> success (OperatingSystemCode ("Win10") )
-                | _ -> failure (new Exception(sprintf "Invalid operatings system code specified. Valid values are: Win7, Win8, Win10."))                
-            | Error ex -> failure (new Exception(sprintf "%s" ex.Message))
-            
-        | operatingSystemCode when System.String.IsNullOrWhiteSpace(operatingSystemCode) -> failure (new ArgumentNullException("Operating system code value cannot be null") :> Exception)
-        | _ -> success (OperatingSystemCode operatingSystemCode)
+                | _ -> failure ((new InvalidOperatingSystemCodeException(operatingSystemCode,String.Empty)) :> Exception)
+
+        match operatingSystemCode with
+        | operatingSystemCode when System.String.IsNullOrWhiteSpace(operatingSystemCode) && defaultToLocal -> 
+            match getOperatingSystemCodeForLocalSystem with
+            | Ok os -> validateOperatingSystemCode os
+            | Error ex -> failure ((new InvalidOperatingSystemCodeException(String.Empty,ex.Message)) :> Exception)
+        | _ -> validateOperatingSystemCode operatingSystemCode
 
     static member create (modelCode : string) =
         let success (value : OperatingSystemCode) = Result.Ok value
--- src/test/DriverTool.Tests/OperatingSystemCodeTests.fs
+++ src/test/DriverTool.Tests/OperatingSystemCodeTests.fs
@@ -11,13 +11,20 @@ module OperatingSystemCodeTests  =
         match operatingSystemCodeResult with
         |Ok operatingSystemCode -> Assert.IsFalse((String.IsNullOrWhiteSpace(operatingSystemCode.Value)), sprintf "Operating system code: %s" operatingSystemCode.Value)
         |Error ex -> Assert.Fail(sprintf "Did not expect to fail. Error: %s" ex.Message)
+    
+    [<Test>]
+    let OperatingSystemCodeTest_Invalid_OperatingSystemCode() =
+        let operatingSystemCodeResult = OperatingSystemCode.create "Win1" false
+        match operatingSystemCodeResult with
+        |Ok os -> Assert.Fail(sprintf "Did expect to fail but did not fail.")
+        |Error ex -> Assert.IsTrue(ex.Message.StartsWith("The operating system code 'Win1' is not valid."))
 
     [<Test>]
     let OperatingSystemCode_Empty_OperatingSystemCode_UseLocalIsFalse() =
-        let operatingSystemCodeResult = ModelCode.create "" false
+        let operatingSystemCodeResult = OperatingSystemCode.create "" false
         match operatingSystemCodeResult with
         |Ok operatingSystemCode -> Assert.IsFalse((String.IsNullOrWhiteSpace(operatingSystemCode.Value)), sprintf "Operating system code: %s" operatingSystemCode.Value)
-        |Error ex -> Assert.IsTrue(ex.Message.StartsWith("Value cannot be null."))
+        |Error ex -> Assert.IsTrue(ex.Message.EndsWith("OperatingSystemCode cannot be null or empty."))
 
     [<Test>]
     let OperatingSystemCode_Empty_OperatingSystemCode_UseLocalIsTrue() =
