b6c877136b355a65ab3ba64ee039bf676c0f3f84
trondr <trondr@outlook.com>
2018-08-15 21:29:20 +0200

    Implement trycatch2 function taking two parameters as input

--- src/app/DriverTool/F.fs
+++ src/app/DriverTool/F.fs
@@ -8,3 +8,9 @@ let tryCatch<'T, 'R> f  (t:'T) : Result<'R, Exception> =
     with
         | ex -> Result.Error ex
 
+let tryCatch2<'T1,'T2, 'R> f  (t1:'T1) (t2:'T2) : Result<'R, Exception> =
+    try
+        Result.Ok (f t1 t2)
+    with
+        | ex -> Result.Error ex
+
