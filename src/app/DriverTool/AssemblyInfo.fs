e035df534d43aad88d6ba2d95b387941f634b539
trondr <trondr@outlook.com>
2018-09-23 21:28:01 +0200

    Add assembly info

---
+++ src/app/DriverTool/AssemblyInfo.fs
@@ -0,0 +1,20 @@
+﻿namespace DriverTool
+
+open System.Reflection
+open System.Runtime.CompilerServices
+open System.Runtime.InteropServices
+[<assembly: AssemblyCompany("github.com/trondr")>]
+[<assembly: AssemblyProduct("DriverTool")>]
+[<assembly: AssemblyName("DriverTool")>]
+[<assembly: AssemblyCopyright("Copyright © <github.com/trondr> 2018")>]
+[<assembly: AssemblyTrademark("Trademark ™")>]
+[<assembly: AssemblyDescription("Driver tool downloads software and drivers for a specific PC model and creates a driver packages that can be installed via SCCM.")>]
+#if DEBUG
+[<assembly: AssemblyConfiguration("Debug")>]
+#else
+[<assembly: AssemblyConfiguration("Release")>]
+#endif
+[<assembly: ComVisible(false)>]
+[<assembly: AssemblyVersion("1.0.0.0")>]
+[<assembly: AssemblyFileVersion("1.0.0.0")>]
+()
