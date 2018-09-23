202703075a0c84d000a0c41a5a867a5a9f4804a9
trondr <trondr@outlook.com>
2018-09-23 20:46:11 +0200

    Configure logging.

--- src/app/DriverTool/Program.fs
+++ src/app/DriverTool/Program.fs
@@ -1,20 +1,30 @@
 ﻿open System
 open DriverTool.Commands
 open NCmdLiner
+open DriverTool
+open Logging
 
-let runCommand args =
+type DriverTool = String
+let logger = getLogger<DriverTool>
+
+let runCommand args =        
     let result = NCmdLiner.CmdLinery.RunEx(typedefof<CommandDefinitions>, args)
     let exitCode = 
         match result.IsSuccess with
             |true -> 0
             |false ->                
-                result.OnFailure(new Action<exn>(fun ex -> System.Console.WriteLine(ex.Message)))|> ignore
-                1
+                result.OnFailure(new Action<exn>(fun ex -> logger.Error(ex.Message)))|> ignore
+                1    
     exitCode
 
 [<EntryPoint>]
 let main argv =
+    configureLogging
+    logger.Info("Start: DriverTool. Command Line: " + Environment.CommandLine)
     let exitCode = runCommand argv
+    logger.Info("Stop: DriverTool. Exit code: " + exitCode.ToString())
+#if DEBUG
     Console.WriteLine("Press any key...")
     Console.ReadLine() |> ignore
+#endif
     exitCode // return an integer exit code
