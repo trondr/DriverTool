586d2273d5889d252f8b686739f6fb3f949a5b3a
trondr <trondr@outlook.com>
2018-10-04 14:57:25 +0200

    Retrieve check sum and file size of installer and readme. Size and check sum will later be used to decide if download is allready done previously.

--- src/app/DriverTool/PackageXml.fs
+++ src/app/DriverTool/PackageXml.fs
@@ -26,10 +26,14 @@ module DriverTool =
             Version:string;
             BaseUrl:string;
             InstallerName:string;
+            InstallerCrc:string;
+            InstallerSize:Int64;
             ExtractCommandLine:string;
             InstallCommandLine:string;
             Category:string;
             ReadmeName:string;
+            ReadmeCrc:string;
+            ReadmeSize:Int64;
             ReleaseDate:string;
         }
 
@@ -39,14 +43,41 @@ module DriverTool =
         let name = packageXElement.Attribute(XName.Get("name")).Value
         let version = packageXElement.Attribute(XName.Get("version")).Value
         let title = (packageXElement.Element(XName.Get("Title")).Descendants(XName.Get("Desc")) |> Seq.head).Value
-        let installerName = packageXElement.Element(XName.Get("Files"))
-                                .Element(XName.Get("Installer"))
-                                .Element(XName.Get("File"))
-                                .Element(XName.Get("Name")).Value
-        let readmeName = packageXElement.Element(XName.Get("Files"))
-                                .Element(XName.Get("Readme"))
-                                .Element(XName.Get("File"))
-                                .Element(XName.Get("Name")).Value
+        let installerName = 
+            packageXElement.Element(XName.Get("Files"))
+                        .Element(XName.Get("Installer"))
+                        .Element(XName.Get("File"))
+                        .Element(XName.Get("Name")).Value
+        let installerCrc =
+            packageXElement.Element(XName.Get("Files"))
+                        .Element(XName.Get("Installer"))
+                        .Element(XName.Get("File"))
+                        .Element(XName.Get("CRC")).Value
+        
+        let installerSize =
+            packageXElement.Element(XName.Get("Files"))
+                        .Element(XName.Get("Installer"))
+                        .Element(XName.Get("File"))
+                        .Element(XName.Get("Size")).Value |> int64
+
+        let readmeName = 
+            packageXElement.Element(XName.Get("Files"))
+                        .Element(XName.Get("Readme"))
+                        .Element(XName.Get("File"))
+                        .Element(XName.Get("Name")).Value
+        
+        let readmeCrc =
+            packageXElement.Element(XName.Get("Files"))
+                        .Element(XName.Get("Readme"))
+                        .Element(XName.Get("File"))
+                        .Element(XName.Get("CRC")).Value
+        
+        let readmeSize =
+            packageXElement.Element(XName.Get("Files"))
+                        .Element(XName.Get("Readme"))
+                        .Element(XName.Get("File"))
+                        .Element(XName.Get("Size")).Value |> int64
+
         let extractCommandLine = 
             match packageXElement.Element(XName.Get("ExtractCommand")) with
             | null -> String.Empty
@@ -67,8 +98,12 @@ module DriverTool =
             Title = title;
             Version = version;
             InstallerName = installerName;
+            InstallerCrc = installerCrc;
+            InstallerSize = installerSize;
             BaseUrl = baseUrl;
             ReadmeName = readmeName;
+            ReadmeCrc = readmeCrc;
+            ReadmeSize = readmeSize;
             ExtractCommandLine = extractCommandLine;
             InstallCommandLine = installCommandLine;                
             Category = category;
