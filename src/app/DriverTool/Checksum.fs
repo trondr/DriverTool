fb9ac75ea8607891dda87276b3fb2202cdfb2d72
trondr <trondr@outlook.com>
2018-10-04 21:43:09 +0200

    Refactoring checksum functions

--- src/app/DriverTool/Checksum.fs
+++ src/app/DriverTool/Checksum.fs
@@ -4,19 +4,37 @@ open System
 open System.IO
 open System.Security.Cryptography
 
-let hasSameFileHash (filePath:string, crc:string) =
-    match(System.IO.File.Exists(filePath)) with
-    | true ->   
-        use hashAlgorithm = 
-            match crc.Length with
-            | 32 -> MD5.Create() :> HashAlgorithm
-            | 40 -> SHA1.Create() :> HashAlgorithm
-            | 64 -> SHA256.Create() :> HashAlgorithm
-            | 96 -> SHA384.Create() :> HashAlgorithm
-            | 128 -> SHA512.Create() :> HashAlgorithm
-            | _  -> SHA512.Create() :> HashAlgorithm
-        let fileHash = File.ReadAllBytes(filePath)|> hashAlgorithm.ComputeHash
-        let fileHashString = 
-            BitConverter.ToString(fileHash).Replace("-", "").ToLower()
-        (crc.ToLower() = fileHashString)        
-    |false -> false
+let getHashAlgorithmFromHashStringLength hashStringLength = 
+    match hashStringLength with
+        | 32 -> MD5.Create() :> HashAlgorithm
+        | 40 -> SHA1.Create() :> HashAlgorithm
+        | 64 -> SHA256.Create() :> HashAlgorithm
+        | 96 -> SHA384.Create() :> HashAlgorithm
+        | 128 -> SHA512.Create() :> HashAlgorithm
+        | _  -> SHA512.Create() :> HashAlgorithm
+
+let computeHash filePath (hashAlgorithm:HashAlgorithm)  =
+    let fileHash = File.ReadAllBytes(filePath)|> hashAlgorithm.ComputeHash
+    fileHash
+
+let fileHashToString fileHash =
+    let fileHashString = 
+        BitConverter.ToString(fileHash).Replace("-", "").ToLower()
+    fileHashString
+
+let hasSameFileHash (filePath:string, crc:string, fileSize:Int64) =
+    let hasSameFileHash = 
+        match(System.IO.File.Exists(filePath)) with
+        | true ->   
+            let actualSize = (new System.IO.FileInfo(filePath)).Length
+            let isSameFileSize = 
+                (fileSize = actualSize)
+            match isSameFileSize with        
+            |true -> 
+                use hashAlgorithm = getHashAlgorithmFromHashStringLength crc.Length                
+                let fileHash = computeHash filePath hashAlgorithm
+                let fileHashString = fileHashToString fileHash                
+                (crc.ToLower() = fileHashString)                
+            | false  -> false
+        |false -> false
+    hasSameFileHash
