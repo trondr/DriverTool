1626d47a8b565eea3640c68fafa2664d14f89bad
trondr <trondr@outlook.com>
2018-10-04 16:27:42 +0200

    Implement hasSameFileHash function

---
+++ src/app/DriverTool/Checksum.fs
@@ -0,0 +1,22 @@
+﻿module Checksum
+
+open System
+open System.IO
+open System.Security.Cryptography
+
+let hasSameFileHash (filePath:string, crc:string) =
+    match(System.IO.File.Exists(filePath)) with
+    | true ->   
+        use hashAlgorithm = 
+            match crc.Length with
+            | 32 -> MD5.Create() :> HashAlgorithm
+            | 40 -> SHA1.Create() :> HashAlgorithm
+            | 64 -> SHA256.Create() :> HashAlgorithm
+            | 96 -> SHA384.Create() :> HashAlgorithm
+            | 128 -> SHA512.Create() :> HashAlgorithm
+            | _  -> SHA512.Create() :> HashAlgorithm
+        let fileHash = File.ReadAllBytes(filePath)|> hashAlgorithm.ComputeHash
+        let fileHashString = 
+            BitConverter.ToString(fileHash).Replace("-", "").ToLower()
+        (crc.ToLower() = fileHashString)        
+    |false -> false
