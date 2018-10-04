94707ccdaa9499765d482300400b4cbfa933fc35
trondr <trondr@outlook.com>
2018-10-04 23:46:42 +0200

    Start implementing extract updates functions

--- src/app/DriverTool/CreateDriverPackage.fs
+++ src/app/DriverTool/CreateDriverPackage.fs
@@ -35,7 +35,15 @@ module CreateDriverPackage =
                 |] |> Array.filter (fun m -> (not (String.IsNullOrWhiteSpace(m)) ) )            
             Result.Error (new Exception(String.Format("Failed to validate one or more input parameters.{0}{1}",Environment.NewLine, String.Join(Environment.NewLine, errorMessages))))
     
-    let getUniqueUpdates (updatesResult : Result<seq<PackageInfo>,Exception>) : Result<seq<PackageInfo>,Exception> =
+    
+    let getUniqueUpdates packageInfos = 
+        let uniqueUpdates = 
+            packageInfos
+            |> Seq.groupBy (fun p -> p.InstallerName)
+            |> Seq.map (fun (k,v) -> v |>Seq.head)
+        uniqueUpdates
+
+    let getUniqueUpdatesR (updatesResult : Result<seq<PackageInfo>,Exception>) : Result<seq<PackageInfo>,Exception> =
         match updatesResult with
         |Error ex -> Result.Error ex
         |Ok u ->             
@@ -66,10 +74,15 @@ module CreateDriverPackage =
 
     let downloadUpdatePlain (downloadJob) =
         match hasSameFileHash (downloadJob.DestinationFile, downloadJob.Checksum, downloadJob.Size) with
-        |false -> downloadFile (downloadJob.SourceUri, downloadJob.DestinationFile, true)
+        |false -> 
+            let downloadResult = 
+                downloadFile (downloadJob.SourceUri, downloadJob.DestinationFile, true)
+            match downloadResult with
+            |Ok s -> Result.Ok downloadJob
+            |Error ex -> Result.Error ex
         |true -> 
             Logging.getLoggerByName("downloadUpdatePlain").Info(String.Format("Destination file '{0}' allready exists", downloadJob.DestinationFile))
-            Result.Ok downloadJob.DestinationFile
+            Result.Ok downloadJob
 
     let downloadUpdate (downloadJob) =
         Logging.debugLoggerResult downloadUpdatePlain (downloadJob)
@@ -88,12 +101,63 @@ module CreateDriverPackage =
         match packageInfos with
         | Ok ps -> downloadUpdates destinationDirectory ps            
         | Error ex -> Result.Error ex
+    
+    let toTitlePostFix (title:string) (version:string) (releaseDate:string) = 
+        nullOrWhiteSpaceGuard title "title"
+        let parts = title.Split('-');
+        let titlePostfix = 
+            match parts.Length with
+            | 0 -> String.Empty
+            | _ -> parts.[parts.Length - 1]
+        toValidDirectoryName (String.Format("{0}_{1}_{2}",titlePostfix,version,releaseDate))
+    
+    open System.Linq
+
+    let toTitlePrefix (title:string) (category:string) (postFixLength: int) = 
+        nullOrWhiteSpaceGuard title "title"
+        nullGuard category "category"
+        let parts = title.Split('-');
+        let partsString =
+            (parts.[0]).AsEnumerable().Take(57 - postFixLength - category.Length).ToArray()
+        let titlePrefix = 
+            category + "_" + new String(partsString);
+        toValidDirectoryName titlePrefix    
+
+    let getPackageFolderName (packageInfo:PackageInfo) =
+        let validDirectoryName = 
+            toValidDirectoryName packageInfo.Title
+        let postfix = 
+            toTitlePostFix validDirectoryName packageInfo.Version packageInfo.ReleaseDate
+        let prefix = 
+            toTitlePrefix validDirectoryName (packageInfo.Category |? String.Empty) postfix.Length
+        let packageFolderName = 
+            String.Format("{0}_{1}",prefix,postfix).Replace("__", "_").Replace("__", "_");
+        packageFolderName
+
+    let extractUpdateToPackageFolder downloadJob packageFolder =
+        Result.Error "Not implemented"
+
+    let extractUpdate (rootDirectory, downloadJob) =
+        let packageFolderName = getPackageFolderName downloadJob.Package
+        let packageFolderDirectoryName = System.IO.Path.Combine(rootDirectory,packageFolderName)
+        Result.Error "Not implemented"
+        
+    let extractUpdates rootDirectory downloadJobs = 
+        downloadJobs
+        |> Seq.map (fun dj -> extractUpdate (rootDirectory, dj))
+        Result.Ok downloadJobs
         
     let createDriverPackageSimple ((model: ModelCode), (operatingSystem:OperatingSystemCode)) = 
-        ExportRemoteUpdates.getRemoteUpdates (model, operatingSystem, true)
-        |> getUnique
-        |> getUniqueUpdates
-        |> downloadUpdatesR (System.IO.Path.GetTempPath())
+        let res = 
+            result {
+                let! packageInfos = ExportRemoteUpdates.getRemoteUpdates (model, operatingSystem, true)
+                let uniquePackageInfos = packageInfos |> Seq.distinct
+                let uniqueUpdates = uniquePackageInfos |> getUniqueUpdates
+                let! updates = downloadUpdates (System.IO.Path.GetTempPath()) uniqueUpdates
+                let! extractedUpdates = extractUpdates "" updates                
+                return extractedUpdates
+            }
+        res
 
     let createDriverPackage ((model: ModelCode), (operatingSystem:OperatingSystemCode)) =
         Logging.debugLoggerResult createDriverPackageSimple ((model: ModelCode), (operatingSystem:OperatingSystemCode))
