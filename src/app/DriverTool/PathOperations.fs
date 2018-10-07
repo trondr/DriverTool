47915725876fe7613d03b088b07892736f753646
trondr <trondr@outlook.com>
2018-10-07 11:10:26 +0200

    Modified combine paths functions to resturn Path object instead of string

--- src/app/DriverTool/PathOperations.fs
+++ src/app/DriverTool/PathOperations.fs
@@ -1,9 +1,11 @@
 ﻿namespace DriverTool
 module PathOperations =
-    let combine2Paths path1 path2 =
-        System.IO.Path.Combine(path1,path2)
-    let combine3Paths path1 path2 path3 =
-        System.IO.Path.Combine(path1,path2,path3)
-    let combine4Paths path1 path2 path3 path4 =
-        System.IO.Path.Combine(path1,path2,path3,path4)
+    let combine2Paths (path1, path2) =
+        Path.create (System.IO.Path.Combine(path1, path2))
+    
+    let combine3Paths (path1, path2, path3) =
+        Path.create (System.IO.Path.Combine(path1, path2, path3))
+    
+    let combine4Paths (path1, path2, path3, path4) =
+        Path.create (System.IO.Path.Combine(path1, path2, path3, path4))
     
