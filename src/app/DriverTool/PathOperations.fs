7a6de8bfd6bc760df8fb76392b18247f3b6c352f
trondr <trondr@outlook.com>
2018-10-07 02:57:11 +0200

    Add combine paths functions

---
+++ src/app/DriverTool/PathOperations.fs
@@ -0,0 +1,9 @@
+﻿namespace DriverTool
+module PathOperations =
+    let combine2Paths path1 path2 =
+        System.IO.Path.Combine(path1,path2)
+    let combine3Paths path1 path2 path3 =
+        System.IO.Path.Combine(path1,path2,path3)
+    let combine4Paths path1 path2 path3 path4 =
+        System.IO.Path.Combine(path1,path2,path3,path4)
+    
