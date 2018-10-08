fdaf7de9180db72ce4def1746064d4b408714d93
trondr <trondr@outlook.com>
2018-10-09 00:47:48 +0200

    Convert ensureFileDoesNotExists function parameters to tuple

--- src/test/DriverTool.Tests/FileOperationTests.fs
+++ src/test/DriverTool.Tests/FileOperationTests.fs
@@ -26,7 +26,7 @@ module FileOperationTests =
     let ensureFileDoesNotExistTest_FileExists() =
         use temporaryFile = new TemporaryFile()
         let path = temporaryFile.Path
-        let actualResult = ensureFileDoesNotExist false path
+        let actualResult = ensureFileDoesNotExist (false, path)
         match actualResult with
         |Ok p -> Assert.Fail((sprintf "The test should have failed. File %s exists" p.Value))
         |Error ex -> Assert.AreEqual(String.Format("File exists: '{0}'", path.Value),ex.Message)
@@ -35,7 +35,7 @@ module FileOperationTests =
     let ensureFileDoesNotExistTest_FileExists_Overwrite() =
         use temporaryFile = new TemporaryFile()
         let path = temporaryFile.Path
-        let actualResult = ensureFileDoesNotExist true path
+        let actualResult = ensureFileDoesNotExist (true, path)
         match actualResult with
         |Ok p -> Assert.AreEqual(path.Value, p.Value)
         |Error ex -> Assert.Fail("Test failed")
@@ -45,7 +45,7 @@ module FileOperationTests =
         use temporaryFile = new TemporaryFile()
         let path = temporaryFile.Path
         System.IO.File.Delete(path.Value)
-        let actualResult = ensureFileDoesNotExist false path
+        let actualResult = ensureFileDoesNotExist (false, path)
         match actualResult with
         |Ok p -> Assert.AreEqual(path.Value, p.Value)
         |Error ex -> Assert.Fail("Test failed")
