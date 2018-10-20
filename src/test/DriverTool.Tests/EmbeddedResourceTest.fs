7124cd1fc12a1a1b02a7eeee887d7bf7028c5f73
trondr <trondr@outlook.com>
2018-10-20 12:48:07 +0200

    Test extraction of embedded resource

---
+++ src/test/DriverTool.Tests/EmbeddedResourceTest.fs
@@ -0,0 +1,23 @@
+﻿namespace DriverTool.Tests
+open NUnit.Framework
+open DriverTool
+
+[<TestFixture>]
+module EmbeddedResourceTest  =
+    open DriverTool
+    open System.Reflection
+    open DriverTool.EmbeddedResouce
+
+    [<Test>]
+    let extractEmbeddedResourceToFile () =
+        let res =
+            result {
+                let! testPath = Path.create @"c:\temp\DpInstExitCode2ExitCode_tst.exe";
+                let! testResourceName = ResourceName.create "DriverTool.Tools.DpInstExitCode2ExitCode.exe"
+                let! resultPath = EmbeddedResouce.extractEmbeddedResourceToFile (testResourceName, testResourceName.GetType().Assembly,testPath) 
+                return resultPath
+            }
+        match res with
+        | Ok p -> Assert.IsTrue(true,System.String.Format("Success was expected"))
+        | Error e -> Assert.IsTrue(false,System.String.Format("Error: {0}",e.Message))
+        
