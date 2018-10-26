2f95fed06fb5215090659b0b56cc30239c1826f3
trondr <trondr@outlook.com>
2018-10-26 15:09:48 +0200

    Add Get-ScriptFolder function

---
+++ src/app/DriverTool/Tools/Functions/Get-ScriptFolder.ps1
@@ -0,0 +1,14 @@
+function Get-ScriptFolder
+{
+    Write-Verbose "Get-ScriptFolder..."    
+    if((Test-Path variable:global:scriptFolder) -and ($null -ne $scriptFolder))
+    {
+        $scriptFolder = $global:scriptFolder
+    }
+    else {
+        throw "Global scriptFolder variable has not been set."
+    }
+    Write-Verbose "Get-ScriptFolder->$scriptFolder"
+    return $scriptFolder
+}
+#TEST: Get-ScriptFolder
