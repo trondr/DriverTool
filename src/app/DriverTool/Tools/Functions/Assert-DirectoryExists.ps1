9689a5a9c5e4a3e267a981ec9fcd17fba1c7908d
trondr <trondr@outlook.com>
2018-10-26 16:06:22 +0200

    Add Assert-DirectoryExists function

---
+++ src/app/DriverTool/Tools/Functions/Assert-DirectoryExists.ps1
@@ -0,0 +1,17 @@
+function Assert-DirectoryExists
+{
+    Param(
+        [ValidateNotNullOrEmpty()]
+        [String]
+        $DirectoryName=$(throw "DirectoryName must be specified."),
+        [String]
+        $Message
+    )
+    if([System.IO.Directory]::Exists($DirectoryName) -eq $false)
+    {
+        $(throw "$Message Error: Directory '$DirectoryName' does not exist.")
+    }    
+}
+#TEST : 
+#Assert-DirectoryExists -DirectoryName "c:\temp\dssfs" -Message "dssfs was not found."
+#Assert-DirectoryExists -DirectoryName "c:\temp" -Message "temp was not found."
