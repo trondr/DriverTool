fc422b04750309d43397af6675cc1f5ea1c8ce74
trondr <trondr@outlook.com>
2018-10-27 16:21:20 +0200

    Trace Get-ScriptFolder function call. Use current directory as scriptfolder if scriptfolder is not defined.

--- src/app/DriverTool/Tools/Functions/Get-ScriptFolder.ps1
+++ src/app/DriverTool/Tools/Functions/Get-ScriptFolder.ps1
@@ -1,14 +1,20 @@
 function Get-ScriptFolder
 {
-    Write-Verbose "Get-ScriptFolder..."    
-    if((Test-Path variable:global:scriptFolder) -and ([string]::IsNullOrWhiteSpace($global:scriptFolder) -eq $false))
-    {
-        $scriptFolder = $global:scriptFolder
+    Trace-FunctionCall -Script {
+        if([string]::IsNullOrWhiteSpace($global:ScriptFolder))
+        {
+            Write-Verbose "Getting script folder from current directory (.)"
+            $global:ScriptFolder = [System.IO.Path]::GetFullPath(".")
+            $scriptFolder = $global:scriptFolder
+        } 
+        if((Test-Path variable:global:scriptFolder) -and ([string]::IsNullOrWhiteSpace($global:scriptFolder) -eq $false))
+        {
+            $scriptFolder = $global:scriptFolder
+        }
+        else {
+            throw "Global scriptFolder variable has not been set."
+        }
+        return $scriptFolder
     }
-    else {
-        throw "Global scriptFolder variable has not been set."
-    }
-    Write-Verbose "Get-ScriptFolder->$scriptFolder"
-    return $scriptFolder
 }
 #TEST: Get-ScriptFolder
