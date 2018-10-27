0a5a4be9d31362f6418e137cd406bcd974578c15
trondr <trondr@outlook.com>
2018-10-27 10:58:11 +0200

    Cache construction of log file path

--- src/app/DriverTool/Tools/Functions/Get-LogFile.ps1
+++ src/app/DriverTool/Tools/Functions/Get-LogFile.ps1
@@ -1,15 +1,11 @@
-function Get-LogFile 
+function Get-LogFile
 {
     Write-Verbose "Get-LogFile..."
-    if((Test-Path variable:global:logFile) -and ($null -ne $logFile))
-    {
-        $global:logFile = $global:logFile
+    $logFile = Get-CacedValue -ValueName LogFile -OnCacheMiss {
+        $logF = [System.IO.Path]::Combine($(Get-LogDirectory),$(Get-LogFileName))
+        $logF
     }
-    else {
-        $global:logFile = [System.IO.Path]::Combine($(Get-LogDirectory),$(Get-LogFileName))    
-    }
-    
     Write-Verbose "Get-LogFile->$logFile"
-    return $global:logFile
+    $logFile
 }
 #TEST: Get-LogFile
