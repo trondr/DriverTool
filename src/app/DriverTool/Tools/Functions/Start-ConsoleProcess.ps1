33c6a3c6eac3f963ff80f47afde50a197b0aa190
trondr <trondr@outlook.com>
2018-10-27 16:01:15 +0200

    Support WorkingDirectory parameter on the Start-ConsoleProcess function

--- src/app/DriverTool/Tools/Functions/Start-ConsoleProcess.ps1
+++ src/app/DriverTool/Tools/Functions/Start-ConsoleProcess.ps1
@@ -4,24 +4,29 @@ function Start-ConsoleProcess {
         [string]
         $FilePath,
         [string]
-        $Arguments
+        $CommandArguments
+        #,
+        # [string]
+        # $WorkingDirectory
     )
-    Write-Host "Start-ConsoleProcess -FilePath '$FilePath' -Arguments '$Arguments'"
-    $pinfo = New-Object System.Diagnostics.ProcessStartInfo
-    $pinfo.FileName = $FilePath
-    $pinfo.RedirectStandardError = $true
-    $pinfo.RedirectStandardOutput = $true
-    $pinfo.UseShellExecute = $false
-    $pinfo.Arguments = $Arguments
-    $p = New-Object System.Diagnostics.Process
-    $p.StartInfo = $pinfo
-    $p.Start() | Out-Null
-    $p.WaitForExit()
-    $stdout = $p.StandardOutput.ReadToEnd()
-    $stderr = $p.StandardError.ReadToEnd()
-    Write-Host "Stdout: $stdout"
-    Write-Host "Stderr: $stderr"
-    Write-Host "Start-ConsoleProcess->$($p.ExitCode)"
-    return $p.ExitCode
+    Trace-FunctionCall -Level INFO -Script{
+        $pinfo = New-Object System.Diagnostics.ProcessStartInfo
+        $pinfo.FileName = $FilePath
+        $pinfo.WorkingDirectory = ""#$WorkingDirectory
+        $pinfo.RedirectStandardError = $true
+        $pinfo.RedirectStandardOutput = $true
+        $pinfo.UseShellExecute = $false
+        $pinfo.Arguments = $CommandArguments
+        $p = New-Object System.Diagnostics.Process
+        $p.StartInfo = $pinfo
+        $p.Start() | Out-Null
+        $p.WaitForExit()
+        $stdout = $p.StandardOutput.ReadToEnd()
+        $stderr = $p.StandardError.ReadToEnd()
+        Write-Host "Stdout: $stdout"
+        Write-Host "Stderr: $stderr"
+        return $p.ExitCode
+    }
 }
-#TEST: Start-ConsoleProcess -FilePath "c:\windows\system32\change.exe" -Arguments "user /install"
+#TEST: 
+Start-ConsoleProcess -FilePath "c:\windows\system32\change.exe" -CommandArguments "user /install"
