d1d38694768969903f485f2dbb9c2e88e2b5a563
trondr <trondr@outlook.com>
2018-10-27 08:56:15 +0200

    Code cleanup

--- src/app/DriverTool/Tools/Functions/Get-LogDirectoryFromInstallXml.ps1
+++
@@ -1,9 +0,0 @@
-function Get-LogDirectoryFromInstallXml
-{
-    Write-Verbose "Get-LogDirectoryFromInstallXml..."
-    $installXml = [xml] $(Get-Content -Path "$(Get-InstallXml)")
-    $logDirectory = [System.Environment]::ExpandEnvironmentVariables($($installXml.configuration.LogDirectory))
-    Write-Verbose "Get-LogDirectoryFromInstallXml->$logDirectory"
-    return $logDirectory
-}
-#TEST: Get-LogDirectoryFromInstallXml
