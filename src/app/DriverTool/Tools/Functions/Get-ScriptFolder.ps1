8b222e846ea5715b44c640cc1982644a272f6b51
trondr <trondr@outlook.com>
2018-10-28 02:10:40 +0200

    Update logging in Get-ScriptFolder function

--- src/app/DriverTool/Tools/Functions/Get-ScriptFolder.ps1
+++ src/app/DriverTool/Tools/Functions/Get-ScriptFolder.ps1
@@ -1,20 +1,20 @@
 function Get-ScriptFolder
 {
-    Trace-FunctionCall -Script {
-        if([string]::IsNullOrWhiteSpace($global:ScriptFolder))
-        {
-            Write-Verbose "Getting script folder from current directory (.)"
-            $global:ScriptFolder = [System.IO.Path]::GetFullPath(".")
-            $scriptFolder = $global:scriptFolder
-        } 
-        if((Test-Path variable:global:scriptFolder) -and ([string]::IsNullOrWhiteSpace($global:scriptFolder) -eq $false))
-        {
-            $scriptFolder = $global:scriptFolder
-        }
-        else {
-            throw "Global scriptFolder variable has not been set."
-        }
-        return $scriptFolder
+    Write-Log -Level DEBUG -Message "Get-ScriptFolder..."
+    if([string]::IsNullOrWhiteSpace($global:ScriptFolder))
+    {
+        Write-Log -Level DEBUG -Message "Getting script folder from current directory (.)"
+        $global:ScriptFolder = [System.IO.Path]::GetFullPath(".")
+        $scriptFolder = $global:scriptFolder
+    } 
+    if((Test-Path variable:global:scriptFolder) -and ([string]::IsNullOrWhiteSpace($global:scriptFolder) -eq $false))
+    {
+        $scriptFolder = $global:scriptFolder
     }
+    else {
+        throw "Global scriptFolder variable has not been set."
+    }
+    Write-Log -Level DEBUG -Message "Get-ScriptFolder->$scriptFolder"
+    return $scriptFolder
 }
 #TEST: Get-ScriptFolder
