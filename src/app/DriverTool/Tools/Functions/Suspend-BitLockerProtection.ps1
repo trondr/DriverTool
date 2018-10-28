323aae50181714d9eecec6382cf1bbec49317ed7
trondr <trondr@outlook.com>
2018-10-28 23:34:47 +0100

    Add Resume-BitLockerProtection function Fix issue with deleting scheduled task in Suspend-BitLockerProtection function.

--- src/app/DriverTool/Tools/Functions/Suspend-BitLockerProtection.ps1
+++ src/app/DriverTool/Tools/Functions/Suspend-BitLockerProtection.ps1
@@ -3,7 +3,7 @@ function Suspend-BitLockerProtection
     Trace-FunctionCall -Script {
         Assert-IsAdministrator -Message "Administrative privileges are required to suspend BitLocker."      
         $managedBdeExitCode = Start-ConsoleProcess -FilePath "$(Get-ManageBdeExe)" -CommandArguments "-protectors -disable C:"
-        Start-ConsoleProcess -FilePath "$(Get-SchTasksExe)" -CommandArguments "/Delete /tn `"$(Get-DriverToolResumeBitLockerProtectionScheduledTaskName)`"" | Out-Null
+        Start-ConsoleProcess -FilePath "$(Get-SchTasksExe)" -CommandArguments "/Delete /tn `"$(Get-DriverToolResumeBitLockerProtectionScheduledTaskName)`" /F" | Out-Null
         Start-ConsoleProcess -FilePath "$(Get-SchTasksExe)" -CommandArguments "/Create /tn `"$(Get-DriverToolResumeBitLockerProtectionScheduledTaskName)`" /XML `"$(Get-DriverToolResumeBitLockerProtectionScheduledTaskXml)`"" | Out-Null
         $managedBdeExitCode
     }
