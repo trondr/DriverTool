c5d285c7be8bd44b364f5940d9100d38c5e7e01e
trondr <trondr@outlook.com>
2018-10-30 20:27:40 +0100

    Add Get-PackageFolderName function Code cleanup, refactoring

--- src/app/DriverTool/Tools/Functions/Get-LocalDriversFolder.ps1
+++ src/app/DriverTool/Tools/Functions/Get-LocalDriversFolder.ps1
@@ -1,14 +1,12 @@
 function Get-LocalDriversFolder{
     Trace-FunctionCall -Script{
-        Get-CacedValue -ValueName "LocalDriversFolder" -OnCacheMiss {
-            $ComputerVendor = $(Get-InstallProperty -PropertyName "ComputerVendor")
-            $ComputerModel = $(Get-InstallProperty -PropertyName "ComputerModel")            
-            $OsShortName = $(Get-InstallProperty -PropertyName "OsShortName")
+        Get-CacedValue -ValueName "LocalDriversFolder" -OnCacheMiss {            
             $WindowsFolder = [System.Environment]::GetFolderPath([System.Environment+SpecialFolder]::Windows)            
-            [System.IO.Path]::Combine($WindowsFolder,"Drivers","$($ComputerVendor)_$($ComputerModel)_$($OsShortName)")
+            [System.IO.Path]::Combine($WindowsFolder,"Drivers",$(Get-PackageFolderName))
         }
     }
 }
 #TEST: 
 #Clear-Cache
 #Get-LocalDriversFolder
+
