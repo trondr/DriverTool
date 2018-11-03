6a7d8d8f4803f9ef3e3af0a9ec69caa6cc125424
trondr <trondr@outlook.com>
2018-11-04 00:14:18 +0100

    Fix issue with hanging process. New version of Start-ConsoleProces is based on code from https://stackoverflow.com/questions/24370814/how-to-capture-process-output-asynchronously-in-powershell

--- src/app/DriverTool/PackageTemplate/Functions/Start-ConsoleProcess.ps1
+++ src/app/DriverTool/PackageTemplate/Functions/Start-ConsoleProcess.ps1
@@ -1,45 +1,98 @@
+#Source: https://stackoverflow.com/questions/24370814/how-to-capture-process-output-asynchronously-in-powershell
 function Start-ConsoleProcess {
-    param (
+    # Runs the specified executable and captures its exit code, stdout
+    # and stderr.
+    # Returns: custom object.
+    param(
         [Parameter(Mandatory=$true)]
-        [string]
-        $FilePath,
-        [string]
-        $CommandArguments,
-        [string]
-        $WorkingDirectory,
-        [string]
-        $LogFile
+        [ValidateNotNullOrEmpty()]
+        [String]$FilePath,
+        [Parameter(Mandatory=$false)]
+        [String[]]$CommandArguments,
+        [Parameter(Mandatory=$false)]
+        [string]$WorkingDirectory,
+        [Parameter(Mandatory=$false)]
+        [String]$sVerb,
+        [Parameter(Mandatory=$false)]
+        [string]$LogFile
     )
-    Trace-FunctionCall -Level INFO -Script{
-        $pinfo = New-Object System.Diagnostics.ProcessStartInfo
-        $pinfo.FileName = $FilePath
-        $pinfo.WorkingDirectory = $WorkingDirectory
-        $pinfo.RedirectStandardError = $true
-        $pinfo.RedirectStandardOutput = $true
-        $pinfo.UseShellExecute = $false
-        $pinfo.Arguments = $CommandArguments
-        $p = New-Object System.Diagnostics.Process
-        $p.StartInfo = $pinfo
-        $p.Start() | Out-Null
-        $p.WaitForExit()
-        $stdout = $p.StandardOutput.ReadToEnd()
-        $stderr = $p.StandardError.ReadToEnd()
-        Write-Log -Level INFO -Message "Stdout: $stdout"
-        if([string]::IsNullOrWhiteSpace($stderr) -eq $false)
-        {
-            Write-Log -Level ERROR -Message "Stderr: $stderr"
+    Trace-FunctionCall -Level INFO -Script {
+        # Setting process invocation parameters.
+        $oPsi = New-Object -TypeName System.Diagnostics.ProcessStartInfo
+        $oPsi.CreateNoWindow = $true
+        $oPsi.UseShellExecute = $false
+        $oPsi.RedirectStandardOutput = $true
+        $oPsi.RedirectStandardError = $true
+        $oPsi.FileName = $FilePath
+        if (! [String]::IsNullOrEmpty($CommandArguments)) {
+            $oPsi.Arguments = $CommandArguments
         }
-        
-        if($LogFile)
-        {
-            $stdout | Out-File -FilePath $($LogFile) -Encoding utf8
-            if([string]::IsNullOrWhiteSpace($stderr) -eq $false)
-            {                
-                $stderr | Out-File -FilePath $($LogFile) -Encoding utf8 -Append
+
+        if (! [String]::IsNullOrEmpty($WorkingDirectory)) {
+            $oPsi.WorkingDirectory = $WorkingDirectory
+        }
+
+        if (! [String]::IsNullOrEmpty($sVerb)) {
+            $oPsi.Verb = $sVerb
+        }
+
+        # Creating process object.
+        $oProcess = New-Object -TypeName System.Diagnostics.Process
+        $oProcess.StartInfo = $oPsi
+
+        # Creating string builders to store stdout and stderr.
+        $oStdOutBuilder = New-Object -TypeName System.Text.StringBuilder
+        $oStdErrBuilder = New-Object -TypeName System.Text.StringBuilder
+
+        # Adding event handers for stdout and stderr.
+        $sScripBlock = {
+            if (! [String]::IsNullOrEmpty($EventArgs.Data)) {
+                $Event.MessageData.AppendLine($EventArgs.Data)
+                # Write-Host $($EventArgs.Data) -ForegroundColor Green
+                # Write-Log -Level INFO -Message $($EventArgs.Data)
+                # if($LogFile)
+                # {
+                #     $($EventArgs.Data) | Out-File -FilePath $($LogFile) -Encoding utf8
+                # }
             }
         }
-        return $p.ExitCode
+        $oStdOutEvent = Register-ObjectEvent -InputObject $oProcess -Action $sScripBlock -EventName 'OutputDataReceived' -MessageData $oStdOutBuilder
+        $oStdErrEvent = Register-ObjectEvent -InputObject $oProcess -Action $sScripBlock -EventName 'ErrorDataReceived' -MessageData $oStdErrBuilder
+
+        # Starting process.
+        [Void]$oProcess.Start()
+        $oProcess.BeginOutputReadLine()
+        $oProcess.BeginErrorReadLine()
+        [Void]$oProcess.WaitForExit()
+        $oProcess.CancelErrorRead()
+        $oProcess.CancelOutputRead()
+
+        # Unregistering events to retrieve process output.
+        Unregister-Event -SourceIdentifier $oStdOutEvent.Name
+        Unregister-Event -SourceIdentifier $oStdErrEvent.Name
+
+        $oResult = New-Object -TypeName PSObject -Property ([Ordered]@{
+            "ExeFile"  = $FilePath;
+            "Args"     = $CommandArguments -join " ";
+            "ExitCode" = $oProcess.ExitCode;
+            "StdOut"   = $oStdOutBuilder.ToString().Trim();
+            "StdErr"   = $oStdErrBuilder.ToString().Trim()
+        })
+        if (! [String]::IsNullOrEmpty($($oResult.StdOut))) {
+            Write-Log -Level INFO -Message $($oResult.StdOut)
+            if($LogFile)
+            {
+                $($oResult.StdErr) | Out-File -FilePath $($LogFile) -Encoding utf8 -Append
+            }
+        }
+        if (! [String]::IsNullOrEmpty($($oResult.StdErr))) {
+            Write-Log -Level ERROR -Message $($oResult.StdErr)
+            if($LogFile)
+            {
+                $($oResult.StdErr) | Out-File -FilePath $($LogFile) -Encoding utf8 -Append
+            }
+        }
+        return $oResult.ExitCode
     }
 }
-#TEST: 
-#Start-ConsoleProcess -FilePath "c:\windows\system32\change.exe" -CommandArguments "user /install"
+#TEST: Start-ConsoleProcess -FilePath "c:\windows\system32\change.exe" -CommandArguments "user /install"
