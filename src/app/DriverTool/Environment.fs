d01e10eae7906a83f99367907a8a33df58242b0f
trondr <trondr@outlook.com>
2018-11-04 16:05:40 +0100

    Add support for "unexpanding" a string back to a string with environment variables. Example: The unexpanded version of  "C:\Users\Public\Logs" is  "%PUBLIC%\Logs"

---
+++ src/app/DriverTool/Environment.fs
@@ -0,0 +1,36 @@
+﻿namespace DriverTool
+
+module Environment=
+    
+    let expandEnvironmentVariables (text:string) =
+        System.Environment.ExpandEnvironmentVariables(text)
+    
+    let replaceValueWithValueName (text:string, value:string, valueName:string) :string =
+        if (value.Length > 1) then
+            let replacedText =
+                text.Replace(value, System.String.Format("%{0}%", valueName))
+            replacedText
+        else
+            text
+
+    let unExpandEnironmentVariables (text:string) :string =
+        let environmentVariables = System.Environment.GetEnvironmentVariables()
+        let keys = 
+            environmentVariables.Keys
+            |> Seq.cast<string>
+        let values = 
+            environmentVariables.Values
+            |> Seq.cast<string>
+        let valueKeyMap = 
+            keys
+            |> Seq.zip values
+            |> Seq.sortBy (fun (v,k) -> -v.Length) //Make sure to replace the longest parts of the text first
+        let mutable unexpandedText = text
+        valueKeyMap
+        |> Seq.map (fun (v,k) -> 
+                unexpandedText <- replaceValueWithValueName (unexpandedText, v, k)
+            )
+        |>Seq.toArray
+        |>ignore
+        unexpandedText
+
