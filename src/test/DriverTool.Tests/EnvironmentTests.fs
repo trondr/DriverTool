5456080367358c3517748e065bc6b54191711338
trondr <trondr@outlook.com>
2018-11-04 16:07:11 +0100

    Add test of unExandEnvironmentVariables function

---
+++ src/test/DriverTool.Tests/EnvironmentTests.fs
@@ -0,0 +1,16 @@
+﻿namespace DriverTool.Tests
+open NUnit.Framework
+open DriverTool
+
+[<TestFixture>]
+module EnvironmentTests =
+    open DriverTool
+    
+    [<Test>]
+    [<TestCase(@"%Public%\Logs",@"%PUBLIC%\Logs")>]
+    let unExpandEnironmentVariablesTests (unexpandedText:string, expectedUnExpandedText:string) =
+        let expandedText = 
+            Environment.expandEnvironmentVariables unexpandedText
+        let actual = Environment.unExpandEnironmentVariables expandedText
+        Assert.AreEqual(expectedUnExpandedText, actual)
+       
