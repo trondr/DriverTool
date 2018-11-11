506cf030de05c63a2c6510865df340ec7d577c73
trondr <trondr@outlook.com>
2018-11-11 01:26:35 +0100

    Fix issue with getOsShortName in util library

--- src/app/DriverTool.Util/OperatingSystemOperations.fs
+++ src/app/DriverTool.Util/OperatingSystemOperations.fs
@@ -3,13 +3,13 @@
 module OperatingSystemOperations =
     open DriverTool.Util.FSharp
     
-    let GetOsShortName =
+    let GetOsShortName () =
         OperatingSystem.getOsShortName
 
-    let IsServer =
+    let IsServer () =
         OperatingSystem.isServer
     
-    let IsX64 =
+    let IsX64 () =
         OperatingSystem.isOperatingSystemX64
 
 
