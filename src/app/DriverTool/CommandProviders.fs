fc71db328a7767840c4dae60c2e198ad80c6c93f
trondr <trondr@outlook.com>
2018-11-11 11:22:19 +0100

    Add package publisher, manufacturer and systemfamily to the command line parameters of the create driver package command. Handle operating system code as os short name

--- src/app/DriverTool/CommandProviders.fs
+++ src/app/DriverTool/CommandProviders.fs
@@ -17,16 +17,18 @@ module CommandProviders =
     let exportRemoteUdateInfo (modelCodeString, operatingSystemString, csvFilePathString, overwrite) =
         Logging.debugLogger exportRemoteUdateInfoSimple (modelCodeString, operatingSystemString, csvFilePathString, overwrite)
     
-    let createDriverPackageSimple (modelCodeString,operatingSystemString,destinationFolder, logDirectory) =
+    let createDriverPackageSimple (packagePublisher,manufacturerString, systemFamilyString,modelCodeString,operatingSystemString,destinationFolder, logDirectory) =
         match (result {
+                let! manufacturer = Manufacturer.create manufacturerString true
+                let! systemFamily = SystemFamily.create systemFamilyString true
                 let! modelCode = ModelCode.create modelCodeString true
                 let! operatingSystemCode = OperatingSystemCode.create operatingSystemString true
                 let! destinationFolderPath = Path.create destinationFolder
-                let! createDriverPackageResult = DriverTool.CreateDriverPackage.createDriverPackage (modelCode, operatingSystemCode, destinationFolderPath, logDirectory)
+                let! createDriverPackageResult = DriverTool.CreateDriverPackage.createDriverPackage (packagePublisher,manufacturer,systemFamily,modelCode, operatingSystemCode, destinationFolderPath, logDirectory)
                 return createDriverPackageResult
             }) with
         | Ok _ -> NCmdLiner.Result.Ok(0)
         | Error ex -> NCmdLiner.Result.Fail<int>(ex)
 
-    let createDriverPackage (modelCodeString,operatingSystemString,destinationFolder,logDirectory) =
-        Logging.debugLogger createDriverPackageSimple (modelCodeString, operatingSystemString, destinationFolder, logDirectory)
+    let createDriverPackage (packagePublisher, manufacturer, systemFamily,modelCodeString,operatingSystemString,destinationFolder,logDirectory) =
+        Logging.debugLogger createDriverPackageSimple (packagePublisher,manufacturer, systemFamily,modelCodeString, operatingSystemString, destinationFolder, logDirectory)
