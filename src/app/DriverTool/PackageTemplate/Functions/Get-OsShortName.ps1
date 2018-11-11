05784aaba14c75e27ec1141f883a80081f242a05
trondr <trondr@outlook.com>
2018-11-11 01:27:47 +0100

    Implement Get-OsShortName function

--- src/app/DriverTool/PackageTemplate/Functions/Get-OsShortName.ps1
+++ src/app/DriverTool/PackageTemplate/Functions/Get-OsShortName.ps1
@@ -1,9 +1,13 @@
 function Get-OsShortName
 {
-    Import-DriverToolUtilLibrary | Out-Null
-    $obj = New-Object 'DriverTool.Util.Class1'
-    Write-Host $obj.X
-    Write-Log -Level ERROR -Message "Get-OsShortName : Not Implemented."
-    "Win10X64"
+    Trace-FunctionCall -Script {
+        Get-CacedValue -ValueName "OsShortName" -OnCacheMiss {
+            Import-DriverToolUtilLibrary | Out-Null
+            [DriverTool.Util.OperatingSystemOperations]::GetOsShortName()
+        }
+    }
 }
-#TEST: Get-OsShortName
+#TEST
+#Clear-Cache
+#$global:VerbosePreference = "Continue"
+#Get-OsShortName
