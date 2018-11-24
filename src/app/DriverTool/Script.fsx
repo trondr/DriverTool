afd8ff2afdb5aafed9cfa5f91bfce632d1713258
trondr <trondr@outlook.com>
2018-11-24 13:00:01 +0100

    Started implementing webparsing, preparing to get download urls for sccm package

--- src/app/DriverTool/Script.fsx
+++ src/app/DriverTool/Script.fsx
@@ -1,4 +1,2 @@
-﻿let getPrefixes count =
-        Array.init count (fun index -> ((index+1)*10).ToString("D3"))
-    
-(getPrefixes 10)
+﻿
+
---
+++ src/test/DriverTool.Tests/WebParsingTests.fs
@@ -0,0 +1,16 @@
+﻿namespace DriverTool.Tests
+open NUnit.Framework
+open System
+open DriverTool
+
+[<TestFixture>]
+module WebParsingTests  =
+    open DriverTool.Util.FSharp
+
+    [<Test>]
+    let getLenovoSccmPackageDownloadUrlTest_Success() =
+        let actualResult = WebParsing.getLenovoSccmPackageDownloadUrl "https://support.lenovo.com/downloads/ds122238"
+        let expected = "https://download.lenovo.com/pccbbs/thinkcentre_drivers/ts_p320tiny_w1064_201806.exe"
+        match actualResult with
+        |Ok actual -> Assert.AreEqual(expected,actual)
+        |Error e -> Assert.Fail(String.Format("{0}", e.Message))
