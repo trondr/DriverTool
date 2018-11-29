3aaf32610655a4f21649e2e959a783f5d364a99d
trondr <trondr@outlook.com>
2018-11-29 22:29:20 +0100

    Add SccmPackageDowloadInfo type

--- src/app/DriverTool/WebParsing.fs
+++ src/app/DriverTool/WebParsing.fs
@@ -2,7 +2,7 @@
 
 module WebParsing =
     let logger = 
-        Logging.getLoggerByName("CreateDriverPackage")
+        Logging.getLoggerByName("WebParsing")
     open System    
 
     let getContentFromWebPage (uri:string)  =  
@@ -14,19 +14,27 @@ module WebParsing =
             let msg = String.Format("Failed to get web content for web page '{0}' due to {1}",uri, ex.Message)
             Result.Error (new System.Exception(msg,ex))
     
+    type SccmPackageDownloadInfo = {
+        ReadmeUrl: string;
+        ReadmeChecksum: string;
+        InstallerUrl:string;
+        InstallerChecksum:string
+    }
+
     open F    
-    
-    let getLenovoSccmPackageDownloadUrl (uri:string) =
+           
+    let getLenovoSccmPackageDownloadInfo (uri:string) =
         let content = getContentFromWebPage uri
         match content with
         |Ok v -> 
-            let exe = 
+            let (exeUrl, exeChecksum) = 
                 match v with
-                |Regex @"((https[s]?):\/\/[^\s]+\.exe).+?<p>SHA-256:(.+?)</p>" [file;na;sha256] -> ("installer",file,sha256)            
-                |_ -> ("exe","","")
-            let txt =
+                |Regex @"((https[s]?):\/\/[^\s]+\.exe).+?<p>SHA-256:(.+?)</p>" [file;na;sha256] -> (file,sha256)            
+                |_ -> ("","")
+            let (txtUrl,txtChecksum) =
                 match v with
-                |Regex @"((https[s]?):\/\/[^\s]+\.txt).+?<p>SHA-256:(.+?)</p>" [file;na;sha256] -> ("readme",file,sha256)
-                |_ -> ("txt","","")
-            Result.Ok [txt;exe]
+                |Regex @"((https[s]?):\/\/[^\s]+\.txt).+?<p>SHA-256:(.+?)</p>" [file;na;sha256] -> (file,sha256)
+                |_ -> ("","")
+            let sccmPackage = {ReadmeUrl = txtUrl; ReadmeChecksum = txtChecksum; InstallerUrl= exeUrl; InstallerChecksum=exeChecksum}
+            Result.Ok sccmPackage
         |Error ex -> Result.Error ex        
