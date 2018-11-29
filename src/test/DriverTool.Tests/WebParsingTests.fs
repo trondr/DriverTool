82e1b5db2f31ee6f6ce2b0b9244e3654465705f5
trondr <trondr@outlook.com>
2018-11-29 22:58:44 +0100

    Add new tests

--- src/test/DriverTool.Tests/WebParsingTests.fs
+++ src/test/DriverTool.Tests/WebParsingTests.fs
@@ -15,13 +15,13 @@ module WebParsingTests  =
 
     [<Test>]    
     [<Apartment(ApartmentState.STA)>]
-    let getLenovoSccmPackageDownloadUrlTest_Success() =
+    [<TestCase("https://support.lenovo.com/downloads/ds122238","https://download.lenovo.com/pccbbs/thinkcentre_drivers/ts_p320tiny_w1064_201806.txt","297ce1fbe0e0dfe4397c1413fe3850211600274356122b44af7d38fd9fcd5be4","https://download.lenovo.com/pccbbs/thinkcentre_drivers/ts_p320tiny_w1064_201806.exe","6aca612b0282e6f24de6aa19173e58c04ed9c480791ccb928cc039378c3eb513")>]
+    [<TestCase("https://support.lenovo.com/downloads/ds112090","https://download.lenovo.com/pccbbs/thinkcentre_drivers/ts_p320tiny_w1064_201806.txt","297ce1fbe0e0dfe4397c1413fe3850211600274356122b44af7d38fd9fcd5be4","https://download.lenovo.com/pccbbs/thinkcentre_drivers/ts_p320tiny_w1064_201806.exe","6aca612b0282e6f24de6aa19173e58c04ed9c480791ccb928cc039378c3eb513")>]
+    let getLenovoSccmPackageDownloadUrlTest_Success(webPageUrl, expectedReadmeUrl, expectedReadmeChecksum, expectedInstallerUrl, expectedInstallerChecksum) =
         verifyAppartmentStateSta |> ignore
         printfn "%s" (System.IntPtr.Size.ToString())
-        let actualResult = WebParsing.getLenovoSccmPackageDownloadInfo "https://support.lenovo.com/downloads/ds122238"
-        let expected = 
-            {ReadmeUrl="https://download.lenovo.com/pccbbs/thinkcentre_drivers/ts_p320tiny_w1064_201806.txt"; ReadmeChecksum="297ce1fbe0e0dfe4397c1413fe3850211600274356122b44af7d38fd9fcd5be4"; 
-             InstallerUrl="https://download.lenovo.com/pccbbs/thinkcentre_drivers/ts_p320tiny_w1064_201806.exe";InstallerChecksum= "6aca612b0282e6f24de6aa19173e58c04ed9c480791ccb928cc039378c3eb513"}
+        let actualResult = WebParsing.getLenovoSccmPackageDownloadInfo webPageUrl
+        let expected = {ReadmeUrl=expectedReadmeUrl; ReadmeChecksum = expectedReadmeChecksum; InstallerUrl = expectedInstallerUrl;InstallerChecksum = expectedInstallerChecksum}
         match actualResult with
         |Ok actual -> Assert.AreEqual(expected,actual)
         |Error e -> Assert.Fail(String.Format("{0}", e.Message))
