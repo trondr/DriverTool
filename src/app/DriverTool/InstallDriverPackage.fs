5f75da2408d81f821e5df3ca3d3e5fa33b47e539
trondr <trondr@outlook.com>
2018-12-03 23:20:15 +0100

    Implement assertIsSupported function Implement installDriverPackageBase function

---
+++ src/app/DriverTool/InstallDriverPackage.fs
@@ -0,0 +1,64 @@
+﻿namespace DriverTool
+
+module InstallDriverPackage =
+    let logger = Logging.getLoggerByName("InstallDriverPackage")
+    
+    open System
+    open InstallXml
+    open DriverTool.Util.FSharp
+    
+    let getInstallXmlPath (driverPackagePath:Path) =
+        Path.create (System.IO.Path.Combine(driverPackagePath.Value,"Install.xml"))
+
+    type SystemInfo={Model:string; OperatingSystem:string}
+    
+    let getSystemInfo =
+        result{
+            let! currentComputerModel = (WmiHelper.getWmiProperty  "Win32_ComputerSystem" "Model")
+            let currentOsShortName = OperatingSystem.getOsShortName
+            return {Model=currentComputerModel;OperatingSystem=currentOsShortName}
+        }
+
+    let assertIsSupported (installConfiguration:InstallConfigurationData) (systemInfo: SystemInfo) = 
+        result {
+            let isSupportedComputerModel = systemInfo.Model.ToUpper().StartsWith(installConfiguration.ComputerModel.ToUpper())
+            let isSupportedOperatingSystem = systemInfo.OperatingSystem.ToUpper().Equals(installConfiguration.OsShortName.ToUpper())
+            let supportedCheckArray = 
+                [|
+                    (isSupportedComputerModel, String.Format("Computer model '{0}' is not supported by this driver package. Supported model: '{1}'.", systemInfo.Model, installConfiguration.ComputerModel));
+                    (isSupportedOperatingSystem,String.Format("Operating system '{0}' is not supported by this driver package. Supported operating system: '{1}'.", systemInfo.OperatingSystem, installConfiguration.OsShortName));
+                |]
+            let notSupportedMessageArray =
+                supportedCheckArray
+                |>Seq.filter(fun (isSupported,_)-> not isSupported)
+                |>Seq.map (fun (_,message)-> message)
+                |>Seq.toArray
+            let isSupportedResult =
+                match notSupportedMessageArray.Length with
+                |0 -> Result.Ok ()
+                |_ -> Result.Error (new Exception(String.Join(" ",notSupportedMessageArray)))
+            return! isSupportedResult
+       }
+
+    let installDriverPackage (driverPackagePath:Path) =
+        result{
+            let! installXmlPath = getInstallXmlPath driverPackagePath
+            let! installConfiguration = InstallXml.loadInstallXml installXmlPath
+            let! systemInfo = getSystemInfo
+            logger.Info("Checking if driver package is supported...")
+            let! isSupported = assertIsSupported installConfiguration systemInfo
+            logger.Info("Driver package is supported: " + isSupported.ToString())
+            //assertIsAdministrator
+            //unregisterApplication
+            //suspendBitLockerProtection
+            //let exitCodeResult = installDrivers driverPackagePath
+            //match exitCodeResult with
+            //|Ok ec -> 
+            //    match ec with
+            //    |0|3010 -> 
+            //        registerApplication
+            //        Result.Ok ec
+            //    |_ -> Result.Error ec
+            return Result.Error (new NotImplementedException():> Exception)
+        }
+        
---
+++ src/test/DriverTool.Tests/InstallDriverPackageTests.fs
@@ -0,0 +1,35 @@
+﻿namespace DriverTool.Tests
+
+open NUnit.Framework
+open DriverTool.InstallDriverPackage
+open DriverTool.InstallXml
+
+[<TestFixture>]
+module InstallDriverPackageTests =
+    
+    [<Test>]
+    [<TestCase("20EQXXXX","WIN10X64","20EQ","WIN10X64","")>]
+    [<TestCase("20EQXXXX","WIN10X64","20EQX","WIN10X64","")>]
+    [<TestCase("20EQXXXX","WIN10X64","20EQY","WIN10X64","Computer model '20EQXXXX' is not supported by this driver package. Supported model: '20EQY'.")>]
+    [<TestCase("20EQXXXX","WIN10X64","20EQX","WIN7X64","Operating system 'WIN10X64' is not supported by this driver package. Supported operating system: 'WIN7X64'.")>]
+    [<TestCase("20EQXXXX","WIN10X64","20EQY","WIN7X64","Computer model '20EQXXXX' is not supported by this driver package. Supported model: '20EQY'. Operating system 'WIN10X64' is not supported by this driver package. Supported operating system: 'WIN7X64'.")>]
+    let assertIsSupportedTest (currentModel,currentOs,packagedModel,packagesOs,expectedErrorMessage) =
+        let systemInfo = {Model=currentModel;OperatingSystem=currentOs}
+        let installConfigurationData = {            
+            ComputerModel=packagedModel;
+            OsShortName=packagesOs;
+            LogDirectory="";
+            LogFileName="";
+            PackageName="";
+            PackageVersion="";
+            Publisher="";
+            ComputerVendor="";            
+            ComputerSystemFamiliy="";            
+            }
+        let actual = assertIsSupported installConfigurationData systemInfo
+        match actual with
+        |Ok _ -> Assert.AreEqual(expectedErrorMessage, "")
+        |Error ex -> Assert.AreEqual(expectedErrorMessage, ex.Message)        
+        ()
+
+
