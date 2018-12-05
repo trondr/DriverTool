6f289868b9bd0bf8a63ff248fe31fa954f8787f1
trondr <trondr@outlook.com>
2018-12-05 22:31:25 +0100

    Start implementing bitlocker functions.

---
+++ src/app/DriverTool/BitLockerOperations.fs
@@ -0,0 +1,24 @@
+﻿namespace DriverTool
+
+module BitLockerOperations=
+        
+    let systemFolder =
+        System.Environment.GetFolderPath(System.Environment.SpecialFolder.System)
+    
+    let schtasksExe =
+        System.IO.Path.Combine(systemFolder,"schtasks.exe")
+    
+    let manageBdeExe =
+        System.IO.Path.Combine(systemFolder,"manage-bde.exe")
+
+    let resumeBitLockerTaskName = 
+        "DriverTool Resume BitLocker Protection"
+    
+    let suspendBitLockerProtection =
+        result{
+            let! exitCode = ProcessOperations.startConsoleProcess (manageBdeExe,"-protectors -disable C:",systemFolder,null,false)
+            //To do implement scheduled task to resume bitlocker
+            return exitCode
+        }        
+
+    
