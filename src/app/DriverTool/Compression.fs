d8b74cb44d36021ee10578838fd3bd741bb8e902
trondr <trondr@outlook.com>
2018-12-11 22:05:16 +0100

    Add support for ziping down a folder

--- src/app/DriverTool/Compression.fs
+++ src/app/DriverTool/Compression.fs
@@ -19,3 +19,14 @@ module Compression =
             return sevenZipExitCode
         }
         
+    let zipFolder (sourceFolderPath:Path, zipFile:Path) =
+        result{
+            let! nonExistingZipFilePath = FileOperations.ensureFileDoesNotExistWithMessage ((String.Format("Zip file '{0}' not found.",zipFile.Value)),false,zipFile)
+            let! existingSourceFolderPath = DirectoryOperations.ensureDirectoryExistsWithMessage ( (String.Format("Cannot zip down a non existing directory '{0}'.",sourceFolderPath.Value)), sourceFolderPath, false)
+            let! parentSourceFolderPath = (DirectoryOperations.getParentFolderPath sourceFolderPath)
+            let! sevenZipExeFilePath = extract7ZipExeFromEmbededResource parentSourceFolderPath
+            let! existing7ZipExeFilePath = FileOperations.ensureFileExistsWithMessage (String.Format("{0} not found.",sevenZipExeFilePath.Value)) sevenZipExeFilePath
+            let! sevenZipExitCode = ProcessOperations.startConsoleProcess (existing7ZipExeFilePath.Value, String.Format("a \"{0}\" \"{1}\\*\"", nonExistingZipFilePath.Value, existingSourceFolderPath.Value),existingSourceFolderPath.Value,null,false)
+            let! cleanupResult = FileOperations.deleteFile existing7ZipExeFilePath
+            return sevenZipExitCode
+        }
