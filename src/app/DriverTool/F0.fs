078b2e2c6d5c66b6371a77238e6f40b2b97aad0e
trondr <trondr@outlook.com>
2018-12-14 11:44:23 +0100

    Moved module F0 and module F to namespace "DriverTool"

--- src/app/DriverTool/F0.fs
+++ src/app/DriverTool/F0.fs
@@ -1,37 +1,39 @@
-﻿[<AutoOpen>]
-module F0
+﻿namespace DriverTool
 
-type ThisAssembly = { Empty:string;}
+[<AutoOpen>]
+module F0=
 
-//Source: https://stackoverflow.com/questions/2920094/how-can-i-convert-between-f-list-and-f-tuple
-let tupleToArray t = 
-    if Microsoft.FSharp.Reflection.FSharpType.IsTuple(t.GetType()) 
-        then Some (Microsoft.FSharp.Reflection.FSharpValue.GetTupleFields t)
-        else None
+    type ThisAssembly = { Empty:string;}
 
-//Source: https://stackoverflow.com/questions/2920094/how-can-i-convert-between-f-list-and-f-tuple
-let listToTuple l =
-    let l' = List.toArray l
-    let types = l' |> Array.map (fun o -> o.GetType())
-    let tupleType = Microsoft.FSharp.Reflection.FSharpType.MakeTupleType types
-    Microsoft.FSharp.Reflection.FSharpValue.MakeTuple (l' , tupleType)
+    //Source: https://stackoverflow.com/questions/2920094/how-can-i-convert-between-f-list-and-f-tuple
+    let tupleToArray t = 
+        if Microsoft.FSharp.Reflection.FSharpType.IsTuple(t.GetType()) 
+            then Some (Microsoft.FSharp.Reflection.FSharpValue.GetTupleFields t)
+            else None
 
-//Source: http://www.fssnip.net/mW/title/memoize-
-let memoize fn =
-  let cache = new System.Collections.Generic.Dictionary<_,_>()
-  (fun x ->
-    match cache.TryGetValue x with
-    | true, v -> v
-    | false, _ -> let v = fn (x)
-                  cache.Add(x,v)
-                  v)
-//Source: http://www.fssnip.net/7UQ/title/Null-Value-guard-active-pattern
-let (|Null|NotNull|) (x : 'T when 'T : not struct) =
-    if obj.ReferenceEquals(x, null) then Null else NotNull x
+    //Source: https://stackoverflow.com/questions/2920094/how-can-i-convert-between-f-list-and-f-tuple
+    let listToTuple l =
+        let l' = List.toArray l
+        let types = l' |> Array.map (fun o -> o.GetType())
+        let tupleType = Microsoft.FSharp.Reflection.FSharpType.MakeTupleType types
+        Microsoft.FSharp.Reflection.FSharpValue.MakeTuple (l' , tupleType)
 
-open System
+    //Source: http://www.fssnip.net/mW/title/memoize-
+    let memoize fn =
+      let cache = new System.Collections.Generic.Dictionary<_,_>()
+      (fun x ->
+        match cache.TryGetValue x with
+        | true, v -> v
+        | false, _ -> let v = fn (x)
+                      cache.Add(x,v)
+                      v)
+    //Source: http://www.fssnip.net/7UQ/title/Null-Value-guard-active-pattern
+    let (|Null|NotNull|) (x : 'T when 'T : not struct) =
+        if obj.ReferenceEquals(x, null) then Null else NotNull x
 
-let rec getAccumulatedExceptionMessages (ex: Exception) =
-            match ex.InnerException with
-            | null -> ex.Message
-            | _ -> ex.Message + " " + (getAccumulatedExceptionMessages ex.InnerException)
+    open System
+
+    let rec getAccumulatedExceptionMessages (ex: Exception) =
+                match ex.InnerException with
+                | null -> ex.Message
+                | _ -> ex.Message + " " + (getAccumulatedExceptionMessages ex.InnerException)
