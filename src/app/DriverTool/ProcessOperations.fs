ff4b3dddbff1ea0445fdb59410bb41e0ad6f0153
trondr <trondr@outlook.com>
2018-12-16 16:23:21 +0100

    Code cleanup. Remove obsolete startProcess functions

--- src/app/DriverTool/ProcessOperations.fs
+++ src/app/DriverTool/ProcessOperations.fs
@@ -12,27 +12,6 @@ module ProcessOperations =
     open System.Threading
     open System.Threading.Tasks
     
-    let startProcessUnsafe (filePath, arguments) =
-        nullGuard filePath "filePath"
-        nullGuard arguments "arguments"
-
-        let currentMethodName = System.Reflection.MethodBase.GetCurrentMethod().Name
-        System.Console.WriteLine("Current method name: " + currentMethodName)
-    
-        let startInfo = new ProcessStartInfo(filePath,arguments)
-        startInfo.WorkingDirectory <- System.IO.Path.GetDirectoryName(filePath)
-        startInfo.CreateNoWindow <- true
-        startInfo.UseShellExecute <- false
-        logger.InfoFormat("Start Process: '{0}' {1}",filePath,arguments)
-        let runningProcess = Process.Start(startInfo)
-        runningProcess.WaitForExit()
-        let exitCode = runningProcess.ExitCode
-        logger.InfoFormat("Exit Process: '{0}' {1}. Exit code: {2}",filePath,arguments,exitCode)
-        exitCode
-    
-    let startProcess (filePath, arguments) =
-        tryCatch startProcessUnsafe (filePath, arguments)
-    
     type ProcessExitData = {FileName:string;Arguments:string;ExitCode:int;StdOutput:string;StdError:string}
 
     let writeProcessExitDataToLog processExitData logFileName appendToLogFile =
