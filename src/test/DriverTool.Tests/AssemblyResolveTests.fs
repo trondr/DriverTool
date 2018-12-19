16ad4b6d6233f32f1756fc49d9064c90d0bd9e36
trondr <trondr@outlook.com>
2018-12-19 11:14:25 +0100

    Add loadAssemblyFromSearchPath test

---
+++ src/test/DriverTool.Tests/AssemblyResolveTests.fs
@@ -0,0 +1,28 @@
+﻿namespace DriverTool.Tests
+
+open NUnit.Framework
+
+[<TestFixture>]
+module AssemblyResolveTests =    
+    open DriverTool.Tests.Init
+    open DriverTool.AssemblyResolver
+    open System.Reflection
+    
+    [<Test>]
+    [<TestCase(true,true)>]
+    [<TestCase(false,false)>]
+    let loadAssemblyFromSearchPathTest (fileExists:bool, isNonNull:bool) =
+
+        let fileExistStub (filePath:string) =
+            fileExists
+
+        let assemblyLoadStub (filePath:string) =
+            match fileExists with
+            |true -> typeof<ThisTestAssembly>.Assembly
+            |false -> null
+        
+        let searchPaths = [|@"c:\temp";@"c:\temp2"|]
+
+        let actual = loadAssemblyFromSearchPathBase (assemblyLoadStub,fileExistStub,searchPaths,new AssemblyName("SomeAssembly"))
+        Assert.IsTrue((actual<>null) = isNonNull)
+        ()
