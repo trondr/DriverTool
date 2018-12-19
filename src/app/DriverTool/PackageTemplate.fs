e3c9623bac4443c32f17d8e793acd3abb8b60474
trondr <trondr@outlook.com>
2018-12-19 17:05:16 +0100

    Ship DriverTool.exe default with prefered 32 bit corflags. This due to integration with 32 bit 3. party assemblies used when creating a driver package. When DriverTool.exe is copied to the the DriverPackage for use during install/uninstall, clear the prefer 32 bit corflag to enable running in 64 bit process.

--- src/app/DriverTool/PackageTemplate.fs
+++ src/app/DriverTool/PackageTemplate.fs
@@ -27,14 +27,30 @@ module PackageTemplate =
             yield System.IO.Path.Combine(exeFileDirectoryName,"FSharp.Core.dll")
         }
 
+    let toResult toValue fromValue  =
+        match fromValue with
+        |Ok _ -> Result.Ok toValue
+        |Error ex -> Result.Error ex
+        
+
     let copyDriverToolToDriverPackage (destinationFolderPath:Path) =
         result{
+            logger.Info("Copy DriverTool.exe to driver package so that it can handle install and uninstall of the driver package.")
             let! driverToolFolderPath = Path.create (System.IO.Path.Combine(destinationFolderPath.Value,"DriverTool"))
             let! existingDriverToolDirectoryPath = DriverTool.DirectoryOperations.ensureDirectoryExists (driverToolFolderPath, true)
-            let! copyResult = 
+            let! driverToolFiles = 
                 getDriverToolFiles
-                |> (DriverTool.FileOperations.copyFiles existingDriverToolDirectoryPath)            
-            return copyResult
+                |> (DriverTool.FileOperations.copyFiles existingDriverToolDirectoryPath)
+            logger.Info("Adjust corflags on the copied DriverTool.exe so that the version of DriverTool.exe in the driver package will prefer to run in 64 bit process on a 64 bit operating system. This is required when installing and uninstalling the driver package.")
+            let! adjustCorFlagResult =
+                driverToolFiles
+                |>Seq.filter(fun p -> p.Value.EndsWith(".exe"))
+                |>Seq.map(fun p -> 
+                            (CorFlags.prefer32BitClear p)
+                            |>toResult p                    
+                    )
+                |>toAccumulatedResult                
+            return adjustCorFlagResult
         }
 
     let extractPackageTemplate (destinationFolderPath:Path) =
