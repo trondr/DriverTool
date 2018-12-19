a81e5cbde93cfbffd7a265b21ce81656a1d7436e
trondr <trondr@outlook.com>
2018-12-20 00:26:01 +0100

    Start implementing support for Dell driver packages.

---
+++ src/test/DriverTool.Tests/DellUpdatesTests.fs
@@ -0,0 +1,23 @@
+﻿namespace DriverTool.Tests
+
+open NUnit.Framework
+
+[<TestFixture>]
+module DellUpdatesTests =
+    open DriverTool
+    open F
+
+    [<Test>]
+    let downloadAndLoadSoftwareCatalogTest () =
+        match(result{
+            let! actual = DriverTool.DellUpdates.downloadAndLoadSoftwareCatalog ()
+            Assert.IsTrue(actual.SoftwareComponents.Length > 0,"Number of software components found in Dell software catalog is 0")
+            Assert.IsNotNull(actual)
+            return actual        
+        }) with
+        |Ok _->Assert.IsTrue(true)
+        |Error ex ->Assert.Fail(ex.Message)
+        
+        
+
+
