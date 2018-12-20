a08605e362b68f79cc450cf0ae423790708a3d0c
trondr <trondr@outlook.com>
2018-12-20 17:36:50 +0100

    Implement functions for validating supported manufacturer

---
+++ src/test/DriverTool.Tests/SystemInfoTests.fs
@@ -0,0 +1,33 @@
+﻿namespace DriverTool.Tests
+
+open NUnit.Framework
+
+[<TestFixture>]
+module SystemInfoTests =
+    open DriverTool
+    open System
+    open DriverTool.SystemInfo
+    
+    [<Test>]
+    [<TestCase("Dell Inc.",true,true,"No error",TestName = "Dell Supported.")>]
+    [<TestCase("Dell Inc.",false,false,"Failed to get Manufacturer from WMI.",TestName = "Dell Wmi failed.")>]
+    [<TestCase("LENOVO",true,true,"No error",TestName = "Lenovo Supported.")>]
+    [<TestCase("LENOVO",false,false,"Failed to get Manufacturer from WMI.",TestName = "Lenovo Wmi failed.")>]
+    [<TestCase("Acer Inc.",true,false,"Manufacturer 'Acer Inc.' is not supported. Supported manufacturers: [Dell|Lenovo]",TestName = "Not supported.")>]
+    let getManufacturerForCurrentSystemBaseTest (wmiManufacturer:string,wmiSucceded:bool, expectedIsSupported:bool, expectedErrorMessage:string) =
+        match (result{
+            
+                    let wmiManufacturerFuncStub () =
+                        match wmiSucceded with
+                        |true -> Result.Ok wmiManufacturer
+                        |false-> Result.Error (new Exception("Failed to get Manufacturer from WMI."))
+
+                    let! actual = DriverTool.SystemInfo.getManufacturerForCurrentSystemBase wmiManufacturerFuncStub
+            return actual
+        }) with
+        |Ok _-> Assert.IsTrue(true)
+        |Error ex ->
+            match expectedIsSupported with
+            |true -> Assert.Fail(ex.Message)
+            |false -> 
+                Assert.AreEqual(expectedErrorMessage,ex.Message)
