77e9b1c8b7d9b6e1cb32e57bc59bcb8063100ca7
trondr <trondr@outlook.com>
2018-12-24 00:08:17 +0100

    Return latest version of  each dell update from list of updates

--- src/test/DriverTool.Tests/DellUpdatesTests.fs
+++ src/test/DriverTool.Tests/DellUpdatesTests.fs
@@ -32,3 +32,31 @@ module DellUpdatesTests =
         }) with
         |Ok _->Assert.IsTrue(true)
         |Error ex ->Assert.Fail(ex.Message)
+
+    open DriverTool.PackageXml
+    open DriverTool
+
+    let packageInfoTestData () =        
+        let packageInfo = {Name = "";Title = "";Version = "";BaseUrl = "";InstallerName = "";InstallerCrc = "";InstallerSize = 0L;ExtractCommandLine = "";InstallCommandLine = "";Category = "";ReadmeName = "";ReadmeCrc = "";ReadmeSize=0L;ReleaseDate= "";PackageXmlName="";}
+        
+        seq{
+            yield {packageInfo with Name="Name1";Version="1.0.20.0"}
+            yield {packageInfo with Name="Name1";Version="2.0.20.0"}
+            yield {packageInfo with Name="Name1";Version="3.0.20.0"}
+            yield {packageInfo with Name="Name2";Version="1.0.20.0"}
+            yield {packageInfo with Name="Name2";Version="2.0.30.0"}
+            yield {packageInfo with Name="Name2";Version="002.01.20.0"}
+        }
+    
+    [<Test>]    
+    let getLatestPackageInfoVersionTest () =
+        let packageInfos = packageInfoTestData ()
+        let actual = DriverTool.DellUpdates.getLatestPackageInfoVersion (packageInfos) |> Seq.toArray
+        Assert.AreEqual(2,actual.Length,"Selected package info should be 1")
+        let actualPackageInfo1 = actual.[0]
+        Assert.AreEqual("Name1",actualPackageInfo1.Name,"Name")
+        Assert.AreEqual("3.0.20.0",actualPackageInfo1.Version,"Version")
+        let actualPackageInfo2 = actual.[1]
+        Assert.AreEqual("Name2",actualPackageInfo2.Name,"Name")
+        Assert.AreEqual("002.01.20.0",actualPackageInfo2.Version,"Version")
+        ()
