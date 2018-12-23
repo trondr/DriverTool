d43b342cd8ec2eb97cac75141553c8081cdd4de2
trondr <trondr@outlook.com>
2018-12-24 00:38:18 +0100

    Code cleanup - remove obsolete code

--- src/app/DriverTool/ExportRemoteUpdates.fs
+++ src/app/DriverTool/ExportRemoteUpdates.fs
@@ -5,39 +5,6 @@ module ExportRemoteUpdates =
     open System
     open System.Text.RegularExpressions
         
-    let validateExportRemoteUdateInfoParameters (modelCode:Result<ModelCode,Exception>, operatingSystemCode:Result<OperatingSystemCode,Exception>, csvPath:Result<Path,Exception>) = 
-        
-        let validationResult = 
-            match modelCode with
-                    |Ok m ->
-                        match operatingSystemCode with
-                        |Ok os ->
-                            match csvPath with
-                            |Ok fp -> Result.Ok (m, os, fp)
-                            |Error ex -> Result.Error ex
-                        |Error ex -> Result.Error ex
-                    |Error ex -> Result.Error ex
-        match validationResult with
-        |Ok _ -> validationResult
-        |Error ex -> 
-            //Accumulate all non-empty error messages into an array
-            let errorMessages = 
-                [|
-                    (match modelCode with
-                    |Error ex -> ex.Message
-                    |Ok m-> String.Empty);
-
-                    (match operatingSystemCode with
-                    |Error ex -> ex.Message
-                    |Ok m-> String.Empty);
-
-                    (match csvPath with
-                    |Error ex -> ex.Message
-                    |Ok m-> String.Empty)            
-
-                |] |> Array.filter (fun m -> (not (String.IsNullOrWhiteSpace(m)) ) )            
-            Result.Error (new Exception(String.Format("Failed to validate one or more input parameters.{0}{1}",Environment.NewLine, String.Join(Environment.NewLine, errorMessages))))
-
     let operatingSystemCode2DownloadableCode (operatingSystemCode: OperatingSystemCode) =
         operatingSystemCode.Value.Replace("X86","").Replace("x86","").Replace("X64","").Replace("x64","")
     
