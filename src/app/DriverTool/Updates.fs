46cb042c0993e915874615e129b7552d56a9a493
trondr <trondr@outlook.com>
2018-12-30 17:58:05 +0100

    Phase out class based Manufacturer type, replaced with discriminated union Manufacturer2 type

--- src/app/DriverTool/Updates.fs
+++ src/app/DriverTool/Updates.fs
@@ -1,21 +1,9 @@
 ﻿namespace DriverTool
 
 open System
-open DriverTool.SystemInfo
 open DriverTool.ManufacturerTypes
 
 module Updates =
-    let getUpdatesFunc (manufacturer:Manufacturer,baseOnLocallyInstalledUpdates:bool) = 
-        match manufacturer.Value with
-        |ManufacturerName.Dell ->        
-            match baseOnLocallyInstalledUpdates with
-            |true -> DellUpdates.getLocalUpdates
-            |false -> DellUpdates.getRemoteUpdates
-        |ManufacturerName.Lenovo ->        
-            match baseOnLocallyInstalledUpdates with
-            |true -> LenovoUpdates.getLocalUpdates
-            |false -> LenovoUpdates.getRemoteUpdates
-        |_ -> raise(new Exception("Manufacturer not supported: " + manufacturer.Value.ToString()))
 
     let getUpdates (manufacturer:Manufacturer2,baseOnLocallyInstalledUpdates:bool) =
         match manufacturer with
@@ -28,27 +16,22 @@ module Updates =
             |true -> LenovoUpdates.getLocalUpdates
             |false -> LenovoUpdates.getRemoteUpdates
 
-    let getSccmPackageFunc (manufacturer:Manufacturer) = 
-        match manufacturer.Value with
-        |ManufacturerName.Dell ->        
-            DellUpdates.getSccmDriverPackageInfo
-        |ManufacturerName.Lenovo ->        
-            LenovoUpdates.getSccmDriverPackageInfo
-        |_ -> raise(new Exception("Manufacturer not supported: " + manufacturer.Value.ToString()))
-    
-    let downloadSccmPackageFunc (manufacturer:Manufacturer) = 
-        match manufacturer.Value with
-        |ManufacturerName.Dell ->        
-            DellUpdates.downloadSccmPackage
-        |ManufacturerName.Lenovo ->        
-            LenovoUpdates.downloadSccmPackage
-        |_ -> raise(new Exception("Manufacturer not supported: " + manufacturer.Value.ToString()))
-
-    let extractSccmPackageFunc (manufacturer:Manufacturer) = 
-        match manufacturer.Value with
-        |ManufacturerName.Dell ->        
-            DellUpdates.extractSccmPackage
-        |ManufacturerName.Lenovo ->        
-            LenovoUpdates.extractSccmPackage
-        |_ -> raise(new Exception("Manufacturer not supported: " + manufacturer.Value.ToString()))
-
+    let getSccmPackageFunc (manufacturer:Manufacturer2) = 
+        match manufacturer with
+        |Manufacturer2.Dell _ -> DellUpdates.getSccmDriverPackageInfo
+        |Manufacturer2.Lenovo _ -> LenovoUpdates.getSccmDriverPackageInfo
+        
+    let downloadSccmPackageFunc (manufacturer:Manufacturer2) = 
+        match manufacturer with
+        |Manufacturer2.Dell _ -> DellUpdates.downloadSccmPackage
+        |Manufacturer2.Lenovo _ -> LenovoUpdates.downloadSccmPackage
+        
+    let extractSccmPackageFunc (manufacturer:Manufacturer2) = 
+        match manufacturer with
+        |Manufacturer2.Dell _ -> DellUpdates.extractSccmPackage
+        |Manufacturer2.Lenovo _->  LenovoUpdates.extractSccmPackage
+        
+    let extractUpdateFunc (manufacturer:Manufacturer2) = 
+        match manufacturer with
+        |Manufacturer2.Dell _ -> DellUpdates.extractUpdate
+        |Manufacturer2.Lenovo _ -> LenovoUpdates.extractUpdate
