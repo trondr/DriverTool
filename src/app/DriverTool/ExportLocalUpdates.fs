16f56a2d2162f15f2cccf44fba7b19154b331900
trondr <trondr@outlook.com>
2018-12-30 16:57:00 +0100

    Code cleanup - refactoring - move code

--- src/app/DriverTool/ExportLocalUpdates.fs
+++ src/app/DriverTool/ExportLocalUpdates.fs
@@ -3,21 +3,15 @@
 
 module ExportLocalUpdates =
     let logger = Logging.getLoggerByName "ExportLocalUpdates"
-    open DriverTool.PackageXml
 
     open System
 
-        
-    open System
-    
     let exportLocalUpdates (csvFilePath:Path) =
         result{       
             let! actualModelCode = ModelCode.create String.Empty true
             let! actualOperatingSystemCode = OperatingSystemCode.create String.Empty true
             let! localUpdates = LenovoUpdates.getLocalUpdates (actualModelCode, actualOperatingSystemCode, true,DriverTool.Configuration.getDriverPackageLogDirectoryPath)
-            let! exportResult = 
-                localUpdates
-                |> DriverTool.ExportRemoteUpdates.exportToCsv csvFilePath
+            let! exportResult = DriverTool.CsvOperations.exportToCsv (csvFilePath, localUpdates)
             logger.Info("Locally installed updates have been exported to file: " + csvFilePath.Value)
             return exportResult            
         }
