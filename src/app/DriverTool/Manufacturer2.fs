cefcb57c61bc4ef3ae78ba3201a5db12672bdb77
trondr <trondr@outlook.com>
2018-12-30 11:51:53 +0100

    Start preparing to refator Manufacturer into a discriminated union type.

---
+++ src/app/DriverTool/Manufacturer2.fs
@@ -0,0 +1,30 @@
+﻿namespace DriverTool
+
+module ManufacturerTypes =
+
+    open System
+    open Microsoft.FSharp.Reflection
+    open System.Text.RegularExpressions
+
+    type Manufacturer2 =
+        |Dell of name:string
+        |Lenovo of name:string
+    
+    let getValidManufacturers () = 
+        FSharpType.GetUnionCases typeof<Manufacturer2>
+        |>Array.map(fun case -> case.Name)
+    
+    let getValidManufacturersString () =
+        String.Join ("|",getValidManufacturers ())
+
+    type InvalidManufacturerException (msg:string) =
+        inherit Exception((sprintf "%s %s" msg (sprintf "Supported manufacturers: %s." (getValidManufacturersString()) )))
+
+    let manufacturerStringToManufacturerUnsafe (manufacturer:string) =
+        match manufacturer with
+        |manufacturerName when Regex.Match(manufacturerName,"Dell",RegexOptions.IgnoreCase).Success -> Manufacturer2.Dell "Dell"
+        |manufacturerName when Regex.Match(manufacturerName,"Lenovo",RegexOptions.IgnoreCase).Success -> Manufacturer2.Lenovo "Lenovo"
+        |_ -> raise (new InvalidManufacturerException(sprintf "Manufacturer '%s' is not supported." manufacturer))
+    
+    let manufacturerStringToManufacturer (manufacturer:string) =
+        tryCatch manufacturerStringToManufacturerUnsafe manufacturer
---
+++ src/test/DriverTool.Tests/ManufacturerTests.fs
@@ -0,0 +1,23 @@
+﻿namespace DriverTool.Tests
+
+open NUnit.Framework
+
+[<TestFixture>]
+module ManufacturerTests =
+    open DriverTool.ManufacturerTypes
+
+    [<Test>]
+    [<TestCase("Dell",true,"Dell \"Dell\"")>]
+    [<TestCase("Lenovo",true,"Lenovo \"Lenovo\"")>]
+    [<TestCase("HP",false,"HP \"HP\"")>]
+    [<TestCase("Microsoft",false,"Microsoft \"Microsoft\"")>]
+    let manufacturerStringToManufacturerTest (manufacturerString:string, isSuccess:bool,expectedTypeName:string) =
+        
+        let actual = manufacturerStringToManufacturer manufacturerString
+        match actual with
+        |Result.Ok m -> 
+            Assert.IsTrue(isSuccess)
+            Assert.AreEqual(expectedTypeName,m.ToString())
+        |Result.Error ex ->
+            Assert.AreEqual(sprintf "Manufacturer '%s' is not supported. Supported manufacturers: Dell|Lenovo." manufacturerString,ex.Message)
+            Assert.IsFalse(isSuccess,"Failure not expected")
