70d9aa3ef04290970c2b357b2d3754b339cb7c16
trondr <trondr@outlook.com>
2018-12-30 10:44:55 +0100

    Code cleanup - move code

--- src/app/DriverTool/CreateDriverPackage.fs
+++ src/app/DriverTool/CreateDriverPackage.fs
@@ -273,41 +273,7 @@ module CreateDriverPackage =
 
     open DriverTool.PackageTemplate
         
-    let getUpdatesFunc (manufacturer:Manufacturer,baseOnLocallyInstalledUpdates:bool) = 
-        match manufacturer.Value with
-        |ManufacturerName.Dell ->        
-            match baseOnLocallyInstalledUpdates with
-            |true -> DellUpdates.getLocalUpdates
-            |false -> DellUpdates.getRemoteUpdates
-        |ManufacturerName.Lenovo ->        
-            match baseOnLocallyInstalledUpdates with
-            |true -> LenovoUpdates.getLocalUpdates
-            |false -> LenovoUpdates.getRemoteUpdates
-        |_ -> raise(new Exception("Manufacturer not supported: " + manufacturer.Value.ToString()))
-
-    let getSccmPackageFunc (manufacturer:Manufacturer) = 
-        match manufacturer.Value with
-        |ManufacturerName.Dell ->        
-            DellUpdates.getSccmDriverPackageInfo
-        |ManufacturerName.Lenovo ->        
-            LenovoUpdates.getSccmDriverPackageInfo
-        |_ -> raise(new Exception("Manufacturer not supported: " + manufacturer.Value.ToString()))
-    
-    let downloadSccmPackageFunc (manufacturer:Manufacturer) = 
-        match manufacturer.Value with
-        |ManufacturerName.Dell ->        
-            DellUpdates.downloadSccmPackage
-        |ManufacturerName.Lenovo ->        
-            LenovoUpdates.downloadSccmPackage
-        |_ -> raise(new Exception("Manufacturer not supported: " + manufacturer.Value.ToString()))
-
-    let extractSccmPackageFunc (manufacturer:Manufacturer) = 
-        match manufacturer.Value with
-        |ManufacturerName.Dell ->        
-            DellUpdates.extractSccmPackage
-        |ManufacturerName.Lenovo ->        
-            LenovoUpdates.extractSccmPackage
-        |_ -> raise(new Exception("Manufacturer not supported: " + manufacturer.Value.ToString()))
+    open DriverTool.Updates
     
     let createDriverPackageBase (packagePublisher:string,manufacturer:Manufacturer,systemFamily:SystemFamily,model: ModelCode, operatingSystem:OperatingSystemCode, destinationFolderPath: Path, baseOnLocallyInstalledUpdates, logDirectory) =             
             result {
