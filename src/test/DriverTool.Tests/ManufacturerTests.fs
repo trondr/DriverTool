4a56d3c43c3bcb287049848e74f2e849be55b8aa
trondr <trondr@outlook.com>
2018-12-30 23:54:12 +0100

    Code cleanup - remove obsolete code

--- src/test/DriverTool.Tests/ManufacturerTests.fs
+++ src/test/DriverTool.Tests/ManufacturerTests.fs
@@ -4,16 +4,22 @@ open NUnit.Framework
 
 [<TestFixture>]
 module ManufacturerTests =
+    open DriverTool
     open DriverTool.ManufacturerTypes
     open System
 
     [<Test>]
     [<TestCase("Dell","Acer",false,true,"Dell \"Dell\"","")>]
+    [<TestCase("Dell Inc.","Acer",false,true,"Dell \"Dell\"","")>]
+
     [<TestCase("Lenovo","Acer",false,true,"Lenovo \"Lenovo\"","")>]
-    [<TestCase("HP","Acer",false,false,"HP \"HP\"","Manufacturer 'HP' is not supported. Supported manufacturers: Dell|Lenovo.")>]
-    [<TestCase("Microsoft","Acer",false,false,"Microsoft \"Microsoft\"","Manufacturer 'Microsoft' is not supported. Supported manufacturers: Dell|Lenovo.")>]
+    [<TestCase("Lenovo Inc.","Acer",false,true,"Lenovo \"Lenovo\"","")>]
+
+
+    [<TestCase("HP","Acer",false,true,"HP \"HP\"","")>]
+    [<TestCase("Microsoft","Acer",false,false,"Microsoft \"Microsoft\"","Manufacturer 'Microsoft' is not supported. Supported manufacturers: Dell|Lenovo|HP.")>]
     [<TestCase("","Dell",true,true,"Dell \"Dell\"","")>]
-    [<TestCase("","",true,false,"Dell \"Dell\"","Current system manufacturer was empty. Supported manufacturers: Dell|Lenovo.")>]
+    [<TestCase("","",true,false,"Dell \"Dell\"","Current system manufacturer was empty. Supported manufacturers: Dell|Lenovo|HP.")>]
     let manufacturerStringToManufacturerBaseTests (manufacturerString:string,currentManufacturerString:string,defaultToLocal:bool, isSuccess:bool,expectedTypeName:string,expectedError) =
         
         let getCurrentManufacturerStub () =
@@ -29,3 +35,30 @@ module ManufacturerTests =
         |Result.Error ex ->
             Assert.AreEqual(expectedError,ex.Message)
             Assert.IsFalse(isSuccess,"Failure not expected")
+
+    
+    [<Test>]
+    [<TestCase("Dell Inc.",true,true,"No error",TestName = "Dell Supported.")>]
+    [<TestCase("Dell Inc.",false,false,"Failed to get Manufacturer from WMI.",TestName = "Dell Wmi failed.")>]
+    [<TestCase("LENOVO",true,true,"No error",TestName = "Lenovo Supported.")>]
+    [<TestCase("LENOVO",false,false,"Failed to get Manufacturer from WMI.",TestName = "Lenovo Wmi failed.")>]
+    [<TestCase("HP",true,true,"No error",TestName = "HP Supported.")>]
+    [<TestCase("HP",false,false,"Failed to get Manufacturer from WMI.",TestName = "HP Wmi failed.")>]
+    [<TestCase("Acer Inc.",true,false,"Manufacturer 'Acer Inc.' is not supported. Supported manufacturers: Dell|Lenovo|HP.",TestName = "Not supported.")>]
+    let getManufacturerForCurrentSystemBaseTest (wmiManufacturer:string,wmiSucceded:bool, expectedIsSupported:bool, expectedErrorMessage:string) =
+        match (result{
+            
+                    let wmiManufacturerFuncStub () =
+                        match wmiSucceded with
+                        |true -> Result.Ok wmiManufacturer
+                        |false-> Result.Error (new Exception("Failed to get Manufacturer from WMI."))
+
+                    let! actual = DriverTool.ManufacturerTypes.manufacturerStringToManufacturerBase ( wmiManufacturerFuncStub,wmiManufacturer,true)
+            return actual
+        }) with
+        |Ok _-> Assert.IsTrue(true)
+        |Error ex ->
+            match expectedIsSupported with
+            |true -> Assert.Fail(ex.Message)
+            |false -> 
+                Assert.AreEqual(expectedErrorMessage,ex.Message)
