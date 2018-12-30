b8fba36220ff1dd7ae2e21c41168bd719cea52c5
trondr <trondr@outlook.com>
2018-12-30 23:54:12 +0100

    Code cleanup - remove obsolete code

--- src/app/DriverTool/SystemInfo.fs
+++ src/app/DriverTool/SystemInfo.fs
@@ -3,45 +3,17 @@
 module SystemInfo=
 
     open F
-    open System.Text.RegularExpressions
     open System
 
-    type ManufacturerName = |Dell = 1 |Lenovo =2
+    open DriverTool.ManufacturerTypes
 
-    let getValidManufacturerNames () =
-        getEnumValuesToString (typeof<ManufacturerName>)
-    
-    let getWmiManufacturerForCurrentSystem () =
-        (WmiHelper.getWmiProperty "Win32_ComputerSystem" "Manufacturer")
-    
-    type WmiManufacturerValueFunc = unit -> Result<string,Exception>
-
-    let manufacturerToManufacturerNameUnsafe (manufacturer:string) =
-        match manufacturer with
-        |manufacturerName when Regex.Match(manufacturerName,"Dell",RegexOptions.IgnoreCase).Success -> ManufacturerName.Dell
-        |manufacturerName when Regex.Match(manufacturerName,"Lenovo",RegexOptions.IgnoreCase).Success -> ManufacturerName.Lenovo
-        |_ -> raise (new Exception(String.Format("Manufacturer '{0}' is not supported. Supported manufacturers: {1}",manufacturer, getValidManufacturerNames())))        
-
-    let manufacturerToManufacturerName (manufacturer:string) =
-        tryCatch manufacturerToManufacturerNameUnsafe manufacturer
-
-    let getManufacturerForCurrentSystemBase (wmiManufacturerValueFunc:WmiManufacturerValueFunc) =
-        result
-                {
-                    let! manufacturer = wmiManufacturerValueFunc ()                    
-                    return! manufacturerToManufacturerName manufacturer                        
-                }
-
-    let getManufacturerForCurrentSystem () =
-        getManufacturerForCurrentSystemBase getWmiManufacturerForCurrentSystem
-
-    let getModelCodeForCurrentSystem () =
+    let getModelCodeForCurrentSystem () :Result<string,Exception> =
         result{
-            let! manufacturer = getManufacturerForCurrentSystem()
+            let! manufacturer = DriverTool.ManufacturerTypes.getManufacturerForCurrentSystem ()
             let! modelCode = 
                 match manufacturer with
-                |ManufacturerName.Dell -> WmiHelper.getWmiProperty "Win32_ComputerSystem" "SystemSKUNumber"
-                |ManufacturerName.Lenovo -> WmiHelper.getWmiProperty "Win32_ComputerSystem" "Model"
-                |_ -> Result.Error (new Exception("Manufacturer not supported."))
+                |Manufacturer2.Dell _ -> WmiHelper.getWmiProperty "Win32_ComputerSystem" "SystemSKUNumber"
+                |Manufacturer2.Lenovo _ -> WmiHelper.getWmiProperty "Win32_ComputerSystem" "Model"
+                |Manufacturer2.HP _ -> WmiHelper.getWmiProperty "Win32_ComputerSystem" "Model"
             return modelCode            
         } 
