9ec0031dd8bce230cae7d071a3c316303b9c2a3f
trondr <trondr@outlook.com>
2018-12-31 15:27:13 +0100

    Implement downloadSccmPackage function

--- src/app/DriverTool/HpUpdates.fs
+++ src/app/DriverTool/HpUpdates.fs
@@ -3,15 +3,33 @@
 module HpUpdates =
     open System
     open PackageXml
+    open DriverTool.Web
+    open DriverTool.HpCatalog
     
     let getRemoteUpdates (modelCode: ModelCode, operatingSystemCode: OperatingSystemCode, overwrite,logDirectory:string) =
         raise (new NotImplementedException("HpUpdates.getRemoteUpdates"))
 
-    let getSccmDriverPackageInfo (modelCode:ModelCode, operatingSystemCode:OperatingSystemCode)  : Result<SccmPackageInfo,Exception> =
-        raise (new NotImplementedException("HpUpdates.getSccmDriverPackageInfo"))
+    let getSccmDriverPackageInfo (modelCode: ModelCode, operatingSystemCode:OperatingSystemCode) =
+        result{
+            let! driverPackCatalogXmlFilePath = downloadDriverPackCatalog()
+            let! sccmPackageInfo = getSccmDriverPackageInfoBase (driverPackCatalogXmlFilePath, modelCode, operatingSystemCode)
+            return sccmPackageInfo
+        }
 
     let downloadSccmPackage (cacheDirectory, sccmPackage:SccmPackageInfo) =
-        raise (new NotImplementedException("HpUpdates.downloadSccmPackage"))
+        result{                        
+            let! installerdestinationFilePath = Path.create (System.IO.Path.Combine(cacheDirectory,sccmPackage.InstallerFileName))
+            let! installerUri = DriverTool.Web.toUri sccmPackage.InstallerUrl
+            let installerDownloadInfo = { SourceUri = installerUri;SourceChecksum = sccmPackage.InstallerChecksum;SourceFileSize = 0L;DestinationFile = installerdestinationFilePath}
+            let! installerInfo = Web.downloadIfDifferent (installerDownloadInfo,false)
+            let installerPath = installerInfo.DestinationFile.Value
+
+            return {
+                InstallerPath = installerPath
+                ReadmePath = String.Empty
+                SccmPackage = sccmPackage;
+            }
+        } 
 
     let extractSccmPackage (downloadedSccmPackage:DownloadedSccmPackageInfo, destinationPath:Path) =
         raise (new NotImplementedException("HpUpdates.extractSccmPackage"))
