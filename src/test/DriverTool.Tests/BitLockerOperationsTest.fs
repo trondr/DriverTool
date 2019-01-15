a2eca5bef636f07c52f0f885291fe7857ee8ea71
trondr <trondr@outlook.com>
2019-01-15 10:24:33 +0100

    Ignore failing tests that require admin privileges.

--- src/test/DriverTool.Tests/BitLockerOperationsTest.fs
+++ src/test/DriverTool.Tests/BitLockerOperationsTest.fs
@@ -4,10 +4,14 @@ open NUnit.Framework
 
 [<TestFixture>]
 module BitLockerOperationsTest =
+    open DriverTool
 
     [<Test>]
     let isBitLockerEnabledTest () =
-        let actual = DriverTool.BitLockerOperations.isBitLockerEnabled()
-        Assert.IsNotNull(actual,"Was null")
-        ()
+        if(not (Requirements.isAdministrator())) then
+            Assert.Inconclusive("This test must be run with admin privileges.")
+        else
+            let actual = DriverTool.BitLockerOperations.isBitLockerEnabled()
+            Assert.IsNotNull(actual,"Was null")
+            ()
 
