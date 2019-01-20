31f78c2b270f3100be05cc8aac89ea826b999af5
trondr <trondr@outlook.com>
2019-01-20 20:07:41 +0100

    Resolve #10 Error result is swallowed when attempting to unzipping Drivers.zip to an already existing folder

--- src/app/DriverTool/Compression.fs
+++ src/app/DriverTool/Compression.fs
@@ -7,8 +7,7 @@ module Compression =
         EmbeddedResouce.extractEmbeddedResouceByFileName ("7za.exe",destinationFolderPath,"7za.exe")
 
     let unzipFile (zipFile:Path, destinationFolderPath:Path) =
-        result{
-            
+        result{            
             let! existingZipFilePath = FileOperations.ensureFileExistsWithMessage (String.Format("Zip file '{0}' not found.",zipFile.Value)) zipFile
             let! existingAndEmptyDestinationFolderPath = DirectoryOperations.ensureDirectoryExistsAndIsEmptyWithMessage (String.Format("Cannot unzip '{0}' to an allready existing folder '{0}'.",zipFile.Value,destinationFolderPath.Value)) destinationFolderPath true
             let! parentDestinationFolderPath = (DirectoryOperations.getParentFolderPath destinationFolderPath)
