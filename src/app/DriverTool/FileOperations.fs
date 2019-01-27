86703d73076b34a50c01bf3f4abd836e35b94276
trondr <trondr@outlook.com>
2019-01-27 02:08:26 +0100

    Code cleanup - remove obsolete code, let build type inference deduce the specified types.

--- src/app/DriverTool/FileOperations.fs
+++ src/app/DriverTool/FileOperations.fs
@@ -3,7 +3,7 @@ open F
 open System
 
 module FileOperations =
-    let deleteFileUnsafe (path: FileSystem.Path)  =
+    let deleteFileUnsafe path  =
         System.IO.File.Delete (FileSystem.pathValue path)
 
     let deleteFile path = 
@@ -24,59 +24,46 @@ module FileOperations =
         | false -> Result.Ok filePath
     
     let ensureFileDoesNotExist (overwrite, filePath) = 
-        ensureFileDoesNotExistWithMessage (String.Empty,overwrite, filePath)
+        ensureFileDoesNotExistWithMessage (String.Empty, overwrite, filePath)
     
-    let ensureFileDoesNotExistR overwrite (path:Result<FileSystem.Path, Exception>) : Result<FileSystem.Path, Exception> = 
-        match path with
-        |Error  ex -> Result.Error ex
-        |Ok p -> ensureFileDoesNotExist (overwrite, p)
-    
-    let ensureFileExists (path:FileSystem.Path) : Result<FileSystem.Path, Exception> = 
+    let ensureFileExists path = 
         match System.IO.File.Exists(FileSystem.pathValue  path) with
         | true -> Result.Ok path            
         | false -> Result.Error (new System.IO.FileNotFoundException(String.Format("File does not exist: '{0}'", FileSystem.pathValue path)) :> Exception)
     
-    let ensureFileExistsWithMessage message (path:FileSystem.Path) = 
+    let ensureFileExistsWithMessage message path = 
         match System.IO.File.Exists(FileSystem.pathValue path) with
         | true -> Result.Ok path            
         | false -> Result.Error (new System.IO.FileNotFoundException(message) :> Exception)
-
-    let ensureFileExistsR (path:Result<FileSystem.Path, Exception>) : Result<FileSystem.Path, Exception> = 
-        match path with
-        |Ok p -> ensureFileExists p
-        |Error ex -> Result.Error ex
-
-    let fileExistsUnsafe (filePath:string) =
-        System.IO.File.Exists(filePath)
     
-    let fileExists (filePath:FileSystem.Path) =
+    let fileExists filePath =
         System.IO.File.Exists(FileSystem.pathValue filePath)
 
     let getFileSize filePath =
         (new System.IO.FileInfo(FileSystem.pathValue filePath)).Length
  
-    let writeContentToFile (filePath:FileSystem.Path) (content:string) =         
+    let writeContentToFile filePath (content:string) =         
         try
             use sw = (new System.IO.StreamWriter(FileSystem.pathValue filePath))
             Result.Ok (sw.Write(content))
         with
         |ex -> Result.Error ex
     
-    let readContentFromFile (filePath:FileSystem.Path) = 
+    let readContentFromFile filePath = 
         try
             use sr = (new System.IO.StreamReader(FileSystem.pathValue filePath))
             Result.Ok (sr.ReadToEnd())
         with
         |ex -> Result.Error ex
     
-    let copyFileUnsafe (sourceFilePath:FileSystem.Path, destinationFilePath:FileSystem.Path, force) =
+    let copyFileUnsafe (sourceFilePath, destinationFilePath, force) =
         System.IO.File.Copy(FileSystem.pathValue sourceFilePath, FileSystem.pathValue destinationFilePath, force)
         destinationFilePath
     
-    let copyFile (sourceFilePath:FileSystem.Path, destinationFilePath:FileSystem.Path, force) =
-        tryCatchWithMessage copyFileUnsafe (sourceFilePath, destinationFilePath, force) (String.Format("Failed to copy file: '{0}'->{1}. ",sourceFilePath,destinationFilePath))
+    let copyFile (sourceFilePath, destinationFilePath, force) =
+        tryCatchWithMessage copyFileUnsafe (sourceFilePath, destinationFilePath, force) (sprintf "Failed to copy file: '%A'->%A. " sourceFilePath destinationFilePath)
 
-    let copyFiles (destinationFolderPath:FileSystem.Path) (files:seq<string>) =
+    let copyFiles (destinationFolderPath) (files:seq<string>) =
         files
         |>Seq.map(fun fp -> 
                     result{
@@ -90,10 +77,10 @@ module FileOperations =
         |>Seq.toArray
         |>toAccumulatedResult
     
-    let ensureFileExtension (path:FileSystem.Path, extension:string) : Result<FileSystem.Path, Exception> = 
+    let ensureFileExtension (path, extension:string) : Result<FileSystem.Path, Exception> = 
         match System.IO.Path.GetExtension(FileSystem.pathValue path).ToLower() with
         | e when e = extension -> Result.Ok path            
-        | _ -> Result.Error (new Exception(String.Format("File does not have extension '{0}': '{1}'",extension, FileSystem.pathValue path)))
+        | _ -> Result.Error (new Exception(sprintf "File does not have extension '%s': '%A'" extension path))
 
     let deleteFileIfExists (filePath:FileSystem.Path) =
         if(System.IO.File.Exists(FileSystem.pathValue filePath)) then
