3e0693a064d581bcce8e9f861b4d3b529e4e8b26
trondr <trondr@outlook.com>
2019-02-10 22:44:43 +0100

    Test XmlToolKit

---
+++ src/test/DriverTool.Tests/XmlToolKitTests.fs
@@ -0,0 +1,36 @@
+﻿namespace DriverTool.Tests
+
+open NUnit.Framework
+
+[<TestFixture>]
+[<Category(TestCategory.UnitTests)>]
+module XmlToolKitTests = 
+
+    open DriverTool.XmlToolKit
+    open System.IO
+
+    [<Test>]
+    let xmlDocLoadSaveTest () =
+        let memoryStreamToString (ms:MemoryStream) =
+            ms.Position <- 0L
+            use reader = new StreamReader(ms)
+            let text = reader.ReadToEnd()
+            text
+        let doc =
+                XDocument (XDeclaration "1.0" "utf-8" "yes") [
+                    XComment "Saved by DriverTool function."
+                    XElement "configuration" [
+                        XElement "Name1" ["Value1"]
+                        XElement "Name2" ["Value2"]
+                        XElement "Name3" ["Value3"]
+                        XElement "Name4" ["Value4"]                        
+                    ]
+                ]
+        let expectedXmlValue = sprintf "%s" (doc.Declaration.ToString() + System.Environment.NewLine + doc.ToString())
+        printfn "%s" expectedXmlValue
+        use ms = doc.Save()
+        let actualXmlValue = memoryStreamToString ms
+        printfn "%s" actualXmlValue
+        Assert.AreEqual(expectedXmlValue, actualXmlValue)
+
+        
