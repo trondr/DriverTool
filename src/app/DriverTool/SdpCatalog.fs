3bc2498bc583fedd2d9e21bbf890b70f4c6a9a5b
trondr <trondr@outlook.com>
2019-03-10 00:38:51 +0100

    Continue implementing parsing of SoftwareDistributionPackage xml

--- src/app/DriverTool/SdpCatalog.fs
+++ src/app/DriverTool/SdpCatalog.fs
@@ -121,6 +121,14 @@ module SdpCatalog =
         |Moderate
         |Low
 
+    let toMsrcSeverity msrcSeverity =
+        match msrcSeverity with
+        |"Critical" -> Result.Ok Critical
+        |"Important" -> Result.Ok Important
+        |"Moderate" -> Result.Ok Moderate
+        |"Low" -> Result.Ok Low
+        |_ -> Result.Error (new Exception(sprintf "Invalid MsRcSeverity '%s'. Valid values: [Critical|Important|Moderate|Low]" msrcSeverity))
+
     type UpdateClassification =
         |Updates
         |SecurityUpdates
@@ -130,20 +138,32 @@ module SdpCatalog =
         |Hotfixes
         |Drivers
 
-    type UpdateSpecificationData=
+    let toUpdateClassification updateClassification =
+        match updateClassification with
+        |"Updates" -> Result.Ok Updates
+        |"SecurityUpdates" -> Result.Ok SecurityUpdates
+        |"FeaturePacks" -> Result.Ok FeaturePacks
+        |"UpdateRollups" -> Result.Ok UpdateRollups
+        |"ServicePacks" -> Result.Ok ServicePacks
+        |"Hotfixes" -> Result.Ok Hotfixes
+        |"Drivers" -> Result.Ok Drivers
+        |_ -> Result.Error (new Exception(sprintf "Invalid UpdateClassification '%s'. Valid values: [Updates|SecurityUpdates|FeaturePacks|UpdateRollups|ServicePacks|Hotfixes|Drivers]" updateClassification))
+
+    type UpdateSpecificData=
         {
             MsrcSeverity:MsrcSeverity
-            UpdateClassificaiton:UpdateClassification
-            SecurityBulletinID:string
-            KBArticleID:string
+            UpdateClassification:UpdateClassification
+            SecurityBulletinID:string option
+            KBArticleID:string option
         }
     
     type SoftwareDistributionPackage =
         {
+            PackageId:string
             Title:string
             Description:string
             ProductName:string
-            //UpdateSpecificationData:UpdateSpecificationData            
+            UpdateSpecificData:UpdateSpecificData            
             //IsInstallable:ApplicabilityRule //An applicability expression that evaluates to true if the package is even relevalant to this machine (e.g., SQL server on XP home). This rule allows the author to specify what prerequisite conditions are necessary for this package to be applicable. If not specified, assummed to be true.
             //IsInstalled:ApplicabilityRule //An applicability expression that evaluates to true if the package is actually installed. This expression is supposed to check for the exact package, not for something that supersedes it.
             //InstallableItem:array<InstallableItem>
@@ -174,9 +194,64 @@ module SdpCatalog =
     let msiNs =
         XNamespace.Get("http://schemas.microsoft.com/wsus/2005/04/CorporatePublishing/Installers/MsiInstallation.xsd")
 
+    let getOptionalAttribute (xElement:XElement) (attributeName:string) =        
+        match xElement with
+        | null -> None
+        |_ -> 
+            let attribute = xElement.Attribute(XName.Get(attributeName))            
+            match attribute with
+            |null -> None
+            |_ -> Some attribute.Value
+
+    let getRequiredAttribute (xElement:XElement) (attributeName:string) =
+        match xElement with
+        | null -> Result.Error (new Exception(sprintf "Element is null. Failed to get attribute name '%s'" attributeName))
+        |_ -> 
+            let attribute = xElement.Attribute(XName.Get(attributeName))            
+            match attribute with
+            |null -> Result.Error (new Exception(sprintf "Attribute '%s' not found on element: %A" attributeName xElement))
+            |_ -> Result.Ok attribute.Value
+    
+    let toResult value errorMessage =
+        match value with
+        | null -> Result.Error (new Exception(errorMessage))
+        | _ -> Result.Ok value
+
+    let getSdpElement (parentXElement:XElement) (elementName:string) =
+        toResult (parentXElement.Element(XName.Get(elementName,sdpNs.NamespaceName))) (sprintf "Element '%s' not found on parent element: '%A'" elementName parentXElement)
+
+    let getSdpElementValue (parentXElement:XElement) (elementName:string) =
+        result{
+            let! element = (getSdpElement parentXElement elementName)
+            return element.Value
+        }
+
+    let getOptionalSdpElementValue (parentXElement:XElement) (elementName:string) =
+        match((getSdpElement parentXElement elementName)) with
+        |Ok v -> Some v.Value
+        |Error _ -> None
+
     let loadSdp (sdpXElement:XElement): Result<SoftwareDistributionPackage, Exception> =
         result{
-            
+            let! localizedPropertiesSdpElement = (getSdpElement sdpXElement "LocalizedProperties")
+            let! title = getSdpElementValue localizedPropertiesSdpElement "Title"                        
+            let! description = getSdpElementValue localizedPropertiesSdpElement "Description"
+            let! propertiesSdpElement = (getSdpElement sdpXElement "Properties")
+            let! packageId = getRequiredAttribute propertiesSdpElement "PackageID"
+            let! productName = getSdpElementValue propertiesSdpElement "ProductName"
 
-            return! Result.Error (new Exception(sprintf "Not implemented: loadSdp: %A" sdpXElement))
+            let! updateSpecificDataSdpElement = (getSdpElement sdpXElement "UpdateSpecificData")
+            let! msrcSeverityString = getRequiredAttribute updateSpecificDataSdpElement "MsrcSeverity"
+            let! msrcSeverity = toMsrcSeverity msrcSeverityString
+            let! updateClassificationString = getRequiredAttribute updateSpecificDataSdpElement "UpdateClassification"
+            let! updateClassification = toUpdateClassification updateClassificationString
+            let securityBulitinId = getOptionalSdpElementValue updateSpecificDataSdpElement "SecurityBulletinID"
+            let kBArticleID = getOptionalSdpElementValue updateSpecificDataSdpElement "KBArticleID"
+            return {
+                    Title = title
+                    Description= description
+                    ProductName = productName
+                    PackageId = packageId
+                    UpdateSpecificData={MsrcSeverity= msrcSeverity;UpdateClassification=updateClassification;SecurityBulletinID=securityBulitinId;KBArticleID=kBArticleID}
+                }            
         }        
--- src/test/DriverTool.Tests/SdpCatalogTests.fs
+++ src/test/DriverTool.Tests/SdpCatalogTests.fs
@@ -7,6 +7,8 @@ open NUnit.Framework
 module SdpCatalogTests =
     open DriverTool
     open System
+    open DriverTool.SdpCatalog
+
     type ThisAssembly = { Empty:string;}
     
 
@@ -27,21 +29,36 @@ module SdpCatalogTests =
             Assert.False(isSuccess,sprintf "Expected success but failed instead: %s" ex.Message)
             Assert.IsTrue(ex.Message.Contains(expectedErrorMessage),"Error message not as expected: " + ex.Message)
 
-        
+    type internal SdpTestData ={SdpTestFile:string;IsSuccess:bool;Expected:SoftwareDistributionPackage;ExpectedErrorMessage:string}
+
+    let internal sdpTestData =
+        [
+            yield {SdpTestFile="0e6cf4ac-2853-48aa-825b-8fe28206575f.sdp";IsSuccess=true;Expected={Title="Realtek High Definition Audio Driver,6.0.1.8454,A02";Description="This package contains the driver for Realtek High-Definition audio codec and is supported on Dell products that run the Windows 10 operating system. Audio driver is the software that helps your operating system to communicate effectively with audio devices such as sound cards and speakers. The package supports Windows 10 Fall Creators Update.";ProductName="Drivers and Applications";PackageId="0e6cf4ac-2853-48aa-825b-8fe28206575f";UpdateSpecificData={MsrcSeverity= MsrcSeverity.Important;UpdateClassification=UpdateClassification.Updates;SecurityBulletinID=Some "99DDD";KBArticleID=Some "99DDD"}};ExpectedErrorMessage="N/A"}
+        ]
+
     [<Test>]
-    [<TestCase("0e6cf4ac-2853-48aa-825b-8fe28206575f.sdp","Realtek High Definition Audio Driver,6.0.1.8454,A02",true,"N/A")>]
-    let loadSdpTests(sdpFileName:string,expectedValue:string,isSuccess:bool,expectedErrorMessage:string) =
+    [<TestCaseSource("sdpTestData")>]
+    //[<TestCase("0e6cf4ac-2853-48aa-825b-8fe28206575f.sdp","Realtek High Definition Audio Driver,6.0.1.8454,A02",true,"N/A")>]
+    let loadSdpTests(sdpTestData:obj) =
+        let sdpTestDataR = (sdpTestData:?>SdpTestData)
         match(result {            
             let! tempDestinationFolderPath = FileSystem.path (PathOperations.getTempPath)            
-            let! sdpFilePath = EmbeddedResouce.extractEmbeddedResouceByFileNameBase (sdpFileName,tempDestinationFolderPath,sdpFileName,typeof<ThisAssembly>.Assembly)
+            let! sdpFilePath = EmbeddedResouce.extractEmbeddedResouceByFileNameBase (sdpTestDataR.SdpTestFile,tempDestinationFolderPath,sdpTestDataR.SdpTestFile,typeof<ThisAssembly>.Assembly)
             let! sdpXDocument = SdpCatalog.loadSdpXDocument sdpFilePath
             let! sdpXElement = SdpCatalog.loadSdpXElement sdpXDocument
             let! actual = SdpCatalog.loadSdp sdpXElement
             return actual
         }) with        
         |Ok v -> 
-            Assert.True(isSuccess, "Expected failed but succeeded instead.")
-            Assert.AreEqual(expectedValue, v.Title,"Product code value not expected")
+            Assert.True(sdpTestDataR.IsSuccess, "Expected failed but succeeded instead.")
+            Assert.AreEqual(sdpTestDataR.Expected.Title, v.Title,"Title")
+            Assert.AreEqual(sdpTestDataR.Expected.Description, v.Description,"Description")
+            Assert.AreEqual(sdpTestDataR.Expected.ProductName, v.ProductName,"ProductName")
+            Assert.AreEqual(sdpTestDataR.Expected.PackageId, v.PackageId,"PackageId")
+            Assert.AreEqual(sdpTestDataR.Expected.UpdateSpecificData.KBArticleID, v.UpdateSpecificData.KBArticleID,"KBArticleID")
+            Assert.AreEqual(sdpTestDataR.Expected.UpdateSpecificData.SecurityBulletinID, v.UpdateSpecificData.SecurityBulletinID,"SecurityBulletinID")
+            Assert.AreEqual(sdpTestDataR.Expected.UpdateSpecificData.UpdateClassification, v.UpdateSpecificData.UpdateClassification,"UpdateClassification")
+            Assert.AreEqual(sdpTestDataR.Expected.UpdateSpecificData.MsrcSeverity, v.UpdateSpecificData.MsrcSeverity,"MsrcSeverity")
         |Error ex -> 
-            Assert.False(isSuccess,sprintf "Expected success but failed instead: %s" ex.Message)
-            Assert.IsTrue(ex.Message.Contains(expectedErrorMessage),"Error message not as expected: " + ex.Message)
+            Assert.False(sdpTestDataR.IsSuccess,sprintf "Expected success but failed instead: %s" ex.Message)
+            Assert.IsTrue(ex.Message.Contains(sdpTestDataR.ExpectedErrorMessage),"Error message not as expected: " + ex.Message)
