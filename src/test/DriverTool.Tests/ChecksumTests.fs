d9f501ed42e53d8dbbf6b00a296939c6130141d1
trondr <trondr@outlook.com>
2019-03-27 00:07:43 +0100

    Download is required if source checksum is not specified Do not complian if source checksum is not specified.

--- src/test/DriverTool.Tests/ChecksumTests.fs
+++ src/test/DriverTool.Tests/ChecksumTests.fs
@@ -3,16 +3,19 @@ open NUnit.Framework
 open DriverTool.Checksum
 
 [<TestFixture>]
-[<Category(TestCategory.UnitTests)>]
 module ChecksumTests =
     open DriverTool
     open System
     
     [<Test>]
-    [<TestCase("File hash should be the same",true,1234,1234,@"c:\temp\somedestinationfile.txt","ABCDE12345ABCDE12345ABCDE12345ABCDE12345","ABCDE12345ABCDE12345ABCDE12345ABCDE12345",true,true,true,true)>]
-    [<TestCase("File hash should not be the same",true,1234,1234,@"c:\temp\somedestinationfile.txt","ABCDE12345ABCDE12345ABCDE12345ABCDE12345","99CDE12345ABCDE12345ABCDE12345ABCDE12399",false,true,true,true)>]
-    [<TestCase("File hash should not be the same due file size difference.",true,0,1234,@"c:\temp\somedestinationfile.txt","NOTUSED","NOTUSED",false,true,true,false)>]
-    [<TestCase("File hash should not be the same due destinaton file not exists.",false,1234,1234,@"c:\temp\somedestinationfile.txt","NOTUSED","NOTUSED",false,true,false,false)>]
+    [<Category(TestCategory.UnitTests)>]
+    [<TestCase("File hash should be the same",true,1234L,1234L,@"c:\temp\somedestinationfile.txt","ABCDE12345ABCDE12345ABCDE12345ABCDE12345","ABCDE12345ABCDE12345ABCDE12345ABCDE12345",true,true,true,true)>]
+    [<TestCase("File hash should not be the same",true,1234L,1234L,@"c:\temp\somedestinationfile.txt","ABCDE12345ABCDE12345ABCDE12345ABCDE12345","99CDE12345ABCDE12345ABCDE12345ABCDE12399",false,true,true,true)>]
+    [<TestCase("File hash should not be the same due file size difference.",true,0L,1234L,@"c:\temp\somedestinationfile.txt","NOTUSED","NOTUSED",false,true,true,false)>]
+    [<TestCase("File hash should not be the same due destinaton file not exists.",false,1234L,1234L,@"c:\temp\somedestinationfile.txt","NOTUSED","NOTUSED",false,true,false,false)>]
+
+    [<TestCase("File hash should be the same if source hash is empty.",true,1234,1234,@"c:\temp\somedestinationfile.txt","","NOTUSED",true,true,true,true)>]
+    [<TestCase("File hash should be the same if source hash is empty and source size is 0.",true,1234L,0L,@"c:\temp\somedestinationfile.txt","","NOTUSED",true,true,true,true)>]
     
     let hasSameFileHashPartialTest (testDescription:string) (destinationFileExists:Boolean) (destinationFileSize:Int64) (sourceFileSize:Int64) (destinationFilePath:string) (sourceFileHash:string) (destinatinationFileHash:string)  (expected:Boolean) (callFileExists:Boolean) (callgetFileSize:Boolean) (callComputeFileHashFromHashLength:Boolean) =
         //Setup
