edd2a93ebc1436b3647a5807e87f6e5749e4ca73
trondr <trondr@outlook.com>
2019-03-31 15:53:48 +0200

    Code cleanup. Rename Manufacturer2 -> Manufacturer.

--- src/app/DriverTool/SystemInfo.fs
+++ src/app/DriverTool/SystemInfo.fs
@@ -12,8 +12,8 @@ module SystemInfo=
             let! manufacturer = DriverTool.ManufacturerTypes.getManufacturerForCurrentSystem ()
             let! modelCode = 
                 match manufacturer with
-                |Manufacturer2.Dell _ -> WmiHelper.getWmiPropertyDefault "Win32_ComputerSystem" "SystemSKUNumber"
-                |Manufacturer2.Lenovo _ -> WmiHelper.getWmiPropertyDefault "Win32_ComputerSystem" "Model"
-                |Manufacturer2.HP _ -> WmiHelper.getWmiProperty "root\WMI" "MS_SystemInformation" "BaseBoardProduct"
+                |Manufacturer.Dell _ -> WmiHelper.getWmiPropertyDefault "Win32_ComputerSystem" "SystemSKUNumber"
+                |Manufacturer.Lenovo _ -> WmiHelper.getWmiPropertyDefault "Win32_ComputerSystem" "Model"
+                |Manufacturer.HP _ -> WmiHelper.getWmiProperty "root\WMI" "MS_SystemInformation" "BaseBoardProduct"
             return modelCode            
         } 
