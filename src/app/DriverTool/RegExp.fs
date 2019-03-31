0a34c17c6f218481938c6e5cf9b29169cd40763c
trondr <trondr@outlook.com>
2019-03-31 11:26:28 +0200

    Add support for matching list of regex patterns. This as a preparation to exclude updates based on patterns given on the command line.

---
+++ src/app/DriverTool/RegExp.fs
@@ -0,0 +1,35 @@
+﻿namespace DriverTool
+
+module RegExp =
+    open System.Text.RegularExpressions
+
+    let toRegexOptions ignoreCase =
+        match ignoreCase with
+        |true -> 
+            System.Text.RegularExpressions.RegexOptions.IgnoreCase
+        |false ->
+            System.Text.RegularExpressions.RegexOptions.None
+
+    /// <summary>
+    /// Convert array of string patterns to array of Regex objects. If ignoreCase is set to true, case will be ignored when matching Regex
+    /// </summary>
+    /// <param name="patterns"></param>
+    /// <param name="ignoreCase"></param>
+    let toRegexPatterns patterns ignoreCase =
+        try
+            let regexOptions = toRegexOptions ignoreCase
+            let regPatterns = 
+                patterns
+                |>Array.map(fun p -> (new Regex(p,regexOptions)))
+            Result.Ok regPatterns                
+        with
+        |ex -> toErrorResult (sprintf "Failed to convert list of patterns to RegEx patterns") (Some ex)
+
+    /// <summary>
+    /// If any of the regular expressions match the input text, return true
+    /// </summary>
+    /// <param name="excludeRegExPatterns"></param>
+    /// <param name="text"></param>
+    let matchAny excludeRegExPatterns text = 
+        excludeRegExPatterns
+        |>Array.exists(fun (p:Regex) -> p.IsMatch(text))
---
+++ src/test/DriverTool.Tests/RegExpTests.fs
@@ -0,0 +1,42 @@
+﻿namespace DriverTool.Tests
+
+module RegExpTests =
+    open NUnit.Framework
+    open DriverTool
+    
+    [<Test>]
+    [<Category(TestCategory.UnitTests)>]
+
+    [<TestCase("Some software for something",true, true, true)>]
+    [<TestCase("Some softare for something",false,true, true)>]
+    [<TestCase("Some driver for something",false,true, true)>]
+    [<TestCase("Some Driver for something",false,true, true)>]
+
+    [<TestCase("Some BIOS for something",true,true, true)>]
+    [<TestCase("Some B1OS for something",true,true, true)>]
+    [<TestCase("Some BOS for something",false,true, true)>]
+
+    [<TestCase("Some software for something",true,true, false)>]
+    [<TestCase("Some softare for something",false,true, false)>]
+
+    [<TestCase("Some software for something",false, false, true)>]
+
+    let excludesTests (text:string, expected:bool, ignoreCase:bool,isSuccess:bool) =
+        match(result
+                {
+                    let patterns = 
+                        match isSuccess with
+                        |true ->
+                            [|@"Soft\ware";@"Utility";@"B.OS";"Firmware"|]
+                        |false -> 
+                            [|@"Soft\ware";@"Utility";@"B.OS";"Firmware";@"\!#%((()"|]
+                    let! regExPatterns = RegExp.toRegexPatterns patterns ignoreCase
+                    let actual = RegExp.matchAny regExPatterns text
+                    Assert.AreEqual(expected,actual,text)
+                    return actual
+                })with
+        |Ok _ -> 
+            Assert.IsTrue(isSuccess)
+        |Error ex -> 
+            printf "%A" ex
+            Assert.IsFalse(isSuccess,(sprintf "Expected success but failed with: %A" ex))
