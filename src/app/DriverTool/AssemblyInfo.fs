ebf8823b7cdc23b4e2cff9ffab4760f6053b371b
trondr <trondr@outlook.com>
2019-09-28 19:29:13 +0200

    DriverTool.1.0.19268.27

--- src/app/DriverTool/AssemblyInfo.fs
+++ src/app/DriverTool/AssemblyInfo.fs
@@ -9,8 +9,8 @@ open System.Runtime.InteropServices
 [<assembly: AssemblyProductAttribute("DriverTool")>]
 [<assembly: AssemblyCompanyAttribute("github/trondr")>]
 [<assembly: AssemblyCopyrightAttribute("Copyright © github/trondr 2018-2019")>]
-[<assembly: AssemblyVersionAttribute("1.0.19268.27")>]
-[<assembly: AssemblyFileVersionAttribute("1.0.19268.27")>]
+[<assembly: AssemblyVersionAttribute("1.0.19271.28")>]
+[<assembly: AssemblyFileVersionAttribute("1.0.19271.28")>]
 [<assembly: ComVisibleAttribute(false)>]
 [<assembly: GuidAttribute("19822aea-c088-455d-b5a5-4738a3a9dba7")>]
 [<assembly: InternalsVisibleToAttribute("DriverTool.Tests")>]
@@ -22,8 +22,8 @@ module internal AssemblyVersionInformation =
     let [<Literal>] AssemblyProduct = "DriverTool"
     let [<Literal>] AssemblyCompany = "github/trondr"
     let [<Literal>] AssemblyCopyright = "Copyright © github/trondr 2018-2019"
-    let [<Literal>] AssemblyVersion = "1.0.19268.27"
-    let [<Literal>] AssemblyFileVersion = "1.0.19268.27"
+    let [<Literal>] AssemblyVersion = "1.0.19271.28"
+    let [<Literal>] AssemblyFileVersion = "1.0.19271.28"
     let [<Literal>] ComVisible = false
     let [<Literal>] Guid = "19822aea-c088-455d-b5a5-4738a3a9dba7"
     let [<Literal>] InternalsVisibleTo = "DriverTool.Tests"
