c3a0c11f5bd27fb4788374f153782c4a2dc5b131
trondr <trondr@outlook.com>
2019-09-28 18:42:34 +0200

    PathOperations: Code cleanup. Add getRandomFileName function

--- src/app/DriverTool/PathOperations.fs
+++ src/app/DriverTool/PathOperations.fs
@@ -6,11 +6,11 @@ module PathOperations =
     let combine3Paths (path1, path2, path3) =
         FileSystem.path (System.IO.Path.Combine(path1, path2, path3))
     
-    let combine4Paths (path1, path2, path3, path4) =
-        FileSystem.path (System.IO.Path.Combine(path1, path2, path3, path4))
-    
     let getTempPath = 
         System.IO.Path.GetTempPath()
+
+    let getRandomFileName() =
+        System.IO.Path.GetRandomFileName()
         
     let getTempFile fileName =
         System.IO.Path.Combine(getTempPath,fileName)
