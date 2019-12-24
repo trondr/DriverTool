a7f26e257715fe7b78a064e8b2999fc09aaf56ea
trondr <trondr@outlook.com>
2019-12-24 13:25:26 +0100

    Feature #32: Start library project and x86 host project.

---
+++ src/app/DriverTool.Library/Messages.fs
@@ -0,0 +1,7 @@
+﻿namespace DriverTool.Library
+
+module Messages =
+    
+    type LenovoSccmPackageInfoRequestMessage() =
+        let mutable sourceUri = ""
+        member this.SourceUri with get() = sourceUri and set(value) = sourceUri <- value
