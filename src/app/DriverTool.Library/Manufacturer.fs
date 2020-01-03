65111de8fb0018d586ffc9c2038be8fa0a89a5b7
trondr <trondr@outlook.com>
2020-01-03 18:16:36 +0100

    Cleanup obsolete debug code.

--- src/app/DriverTool.Library/Manufacturer.fs
+++ src/app/DriverTool.Library/Manufacturer.fs
@@ -10,10 +10,7 @@ module ManufacturerTypes =
     type Manufacturer =
         |Dell of name:string
         |Lenovo of name:string
-        |HP of name:string
-#if DEBUG
-        |Unknown of name:string
-#endif
+        |HP of name:string        
 
     let getValidManufacturers () = 
         FSharpType.GetUnionCases typeof<Manufacturer>
@@ -39,9 +36,6 @@ module ManufacturerTypes =
         |manufacturerName when Regex.Match(manufacturerName,"Dell",RegexOptions.IgnoreCase).Success -> Manufacturer.Dell "Dell"
         |manufacturerName when Regex.Match(manufacturerName,"Lenovo",RegexOptions.IgnoreCase).Success -> Manufacturer.Lenovo "Lenovo"
         |manufacturerName when Regex.Match(manufacturerName,"HP",RegexOptions.IgnoreCase).Success -> Manufacturer.HP "HP"
-#if DEBUG
-        |manufacturerName when Regex.Match(manufacturerName,"System Manufacturer",RegexOptions.IgnoreCase).Success -> Manufacturer.Unknown "Unknown"
-#endif
         |_ -> raise (new InvalidManufacturerException(sprintf "Manufacturer '%s' is not supported." manufacturer))
      
     let manufacturerStringToManufacturerBase (wmiManufacturerValueFunc:WmiManufacturerValueFunc,manufacturer:string,defaultToLocal) =
@@ -54,9 +48,6 @@ module ManufacturerTypes =
         match manufacturer with
         |Dell name -> name
         |Lenovo name -> name
-        |HP name -> name
-#if DEBUG
-        |Unknown name -> name
-#endif    
+        |HP name -> name    
     let getManufacturerForCurrentSystem () = 
         manufacturerStringToManufacturer ("",true)
