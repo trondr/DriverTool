f658b00821abe25a166c2b19935717227f6500fc
trondr <trondr@outlook.com>
2020-01-26 16:34:12 +0100

    Fix #33 Invalid meta data in https://download.lenovo.com/cdrt/td/catalog.xml cause DriverTool to crash

---
+++ src/test/DriverTool.Tests/LenovoCatalogXmlTests.fs
@@ -0,0 +1,19 @@
+﻿namespace DriverTool.Tests
+
+module LenovoCatalogXmlTests =
+    
+    open NUnit.Framework
+
+    [<Test>]
+    [<TestCase("201905a","20190501")>]
+    [<TestCase("201906","20190601")>]
+    [<Category(TestCategory.UnitTests)>]
+    let toDateTests (stringDate, expectedDate) =                
+        let actual = DriverTool.LenovoCatalogXml.toDate (Some stringDate)
+        match actual with
+        |Some d ->
+            let actualDate= (sprintf "%i%02i%02i" d.Year d.Month d.Day)
+            Assert.AreEqual(expectedDate,actualDate)
+        |None->
+            Assert.Fail("Returned date was None.")
+
