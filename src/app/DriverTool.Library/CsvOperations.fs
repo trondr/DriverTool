7cc1dab722200a0bfb36603c1bb56a05daa6e054
trondr <trondr@outlook.com>
2020-01-28 00:14:12 +0100

    Update 3. party libraries

--- src/app/DriverTool.Library/CsvOperations.fs
+++ src/app/DriverTool.Library/CsvOperations.fs
@@ -2,9 +2,11 @@ namespace DriverTool.Library
 
 module CsvOperations =
         
+    open System.Globalization
+
     let exportToCsvUnsafe (csvFilePath:FileSystem.Path, records) =
         use sw = new System.IO.StreamWriter(FileSystem.pathValue csvFilePath)
-        use csv = new CsvHelper.CsvWriter(sw)
+        use csv = new CsvHelper.CsvWriter(sw, CultureInfo.InvariantCulture)
         csv.Configuration.Delimiter <- ";"
         csv.WriteRecords(records)
         csvFilePath
