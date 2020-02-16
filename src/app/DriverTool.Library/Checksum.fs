d035e4637692923785ea625c676005fd213895e2
trondr <trondr@outlook.com>
2020-02-17 00:22:43 +0100

    Prepare checksum functions, with optional parameters.

--- src/app/DriverTool.Library/Checksum.fs
+++ src/app/DriverTool.Library/Checksum.fs
@@ -44,6 +44,49 @@ module Checksum=
     let computeFileHashFromHashLength filePath hashLength =
         genericLogger LogLevel.Debug computeFileHashFromHashLengthBase filePath hashLength
  
+    let hasSameFileHash'' (logger:Common.Logging.ILog) checkFileExists getFileSize computeFileHashFromHashLength (destinationFile:FileSystem.Path, sourceFileHash:string option, sourceFileSize:Int64 option) =
+        logger.Debug( new Msg(fun m -> m.Invoke(sprintf "Checking if source and destination file (%A) has the same file hash (%A)." destinationFile sourceFileHash)|>ignore))
+        let destinationFileExists = checkFileExists destinationFile        
+        match destinationFileExists with
+        | true ->
+            logger.Debug( new Msg(fun m -> m.Invoke(sprintf "Destination file (%A) exists. Check if file hash is the same." destinationFile)|>ignore))
+            let doCompareHash =
+                match sourceFileSize with
+                |Some sourceSize ->
+                    let destinationSize = getFileSize destinationFile
+                    let fileSizeIsEqual = (sourceSize = destinationSize)
+                    logger.Debug( new Msg(fun m -> m.Invoke(sprintf "Comparing source and destination file size. Is equal: %b." fileSizeIsEqual)|>ignore))
+                    fileSizeIsEqual
+                |None -> 
+                    logger.Debug( new Msg(fun m -> m.Invoke(sprintf "Source file size has not been specified, force compare of source and destination hash.")|>ignore))
+                    true
+            let fileHashIsEqual =
+                match doCompareHash with
+                |false ->                     
+                    logger.Debug( new Msg(fun m -> m.Invoke(sprintf "File size was not equal, implying that file hash can not be equal. No neeed to invoke expensive computation of file hash. hasSameFileHash->false.")|>ignore))
+                    false 
+                |true ->                    
+                    logger.Debug( new Msg(fun m -> m.Invoke(sprintf "File size is equal. Invoke computation of file hash to check if source file and destination file has the same file hash.")|>ignore))
+                    match sourceFileHash with
+                    |Some sh ->                        
+                        let sourceHash = sh|>toLower
+                        let destinationHash = computeFileHashFromHashLength destinationFile sourceHash.Length
+                        logger.Debug( new Msg(fun m -> m.Invoke(sprintf "Comparing destination file (%A) hash [%s] and source file hash [%s]..." destinationFile destinationHash sourceHash)|>ignore))
+                        (sourceHash = destinationHash)
+                    |None -> 
+                        logger.Debug( new Msg(fun m -> m.Invoke(sprintf "Source hash has not been specified. hasSameFileHash->false")|>ignore))
+                        false                    
+            fileHashIsEqual
+        | false ->            
+            logger.Debug( new Msg(fun m -> m.Invoke(sprintf "Destination file (%A) does not exist. hasSameFileHash->false." destinationFile)|>ignore))
+            false
+
+    let hasSameFileHash' (filePath:FileSystem.Path, sourceFileHash:string option, fileSize:Int64 option) =
+        hasSameFileHash'' logger fileExists getFileSize computeFileHashFromHashLength (filePath, sourceFileHash, fileSize)
+
+    let hasSameFileHash2 (filePath:FileSystem.Path, crc:string option, fileSize:Int64 option) =
+        DriverTool.Library.Logging.genericLogger LogLevel.Debug hasSameFileHash' (filePath, crc, fileSize)
+        
     let hasSameFileHashPartial fileExists getFileSize computeFileHashFromHashLength (destinationFilePath:FileSystem.Path, sourceFileHash:string, sourceFileSize:Int64) =
             match(fileExists destinationFilePath) with
             | true ->   
@@ -51,7 +94,7 @@ module Checksum=
                 let isSameFileSize = 
                     (sourceFileSize = destinationFileSize) || (sourceFileSize = 0L)
                 match isSameFileSize with        
-                |true ->              
+                |true ->                                  
                     let destinationHash = (computeFileHashFromHashLength destinationFilePath sourceFileHash.Length) |> toLower
                     let sourceHash = sourceFileHash|>toLower
                     logger.Debug( new Msg(fun m -> m.Invoke(sprintf "Comparing destination file (%s) hash [%s] and source file hash [%s]..." (FileSystem.pathValue destinationFilePath) destinationHash sourceHash)|>ignore))
--- src/test/DriverTool.Tests/ChecksumTests.fs
+++ src/test/DriverTool.Tests/ChecksumTests.fs
@@ -7,15 +7,44 @@ module ChecksumTests =
     open DriverTool.Library.Checksum
     open DriverTool.Library
     
+    module DestinationFileExists = 
+        [<Literal>]
+        let True = true
+        [<Literal>]
+        let False = false
+
+    module Expected = 
+        [<Literal>]
+        let True = true
+        [<Literal>]
+        let False = false
+
+    module CallFileExists = 
+        [<Literal>]
+        let True = true
+        [<Literal>]
+        let False = false
+
+    module CallGetFileSize = 
+        [<Literal>]
+        let True = true
+        [<Literal>]
+        let False = false
+
+    module CallComputeFileHashFromHashLength = 
+        [<Literal>]
+        let True = true
+        [<Literal>]
+        let False = false
+
     [<Test>]
     [<Category(TestCategory.UnitTests)>]
-    [<TestCase("File hash should be the same",true,1234L,1234L,@"c:\temp\somedestinationfile.txt","ABCDE12345ABCDE12345ABCDE12345ABCDE12345","ABCDE12345ABCDE12345ABCDE12345ABCDE12345",true,true,true,true)>]
-    [<TestCase("File hash should not be the same",true,1234L,1234L,@"c:\temp\somedestinationfile.txt","ABCDE12345ABCDE12345ABCDE12345ABCDE12345","99CDE12345ABCDE12345ABCDE12345ABCDE12399",false,true,true,true)>]
-    [<TestCase("File hash should not be the same due file size difference.",true,0L,1234L,@"c:\temp\somedestinationfile.txt","NOTUSED","NOTUSED",false,true,true,false)>]
-    [<TestCase("File hash should not be the same due destinaton file not exists.",false,1234L,1234L,@"c:\temp\somedestinationfile.txt","NOTUSED","NOTUSED",false,true,false,false)>]
-
-    [<TestCase("File hash should be the same if source hash is empty.",true,1234,1234,@"c:\temp\somedestinationfile.txt","","NOTUSED",true,true,true,true)>]
-    [<TestCase("File hash should be the same if source hash is empty and source size is 0.",true,1234L,0L,@"c:\temp\somedestinationfile.txt","","NOTUSED",true,true,true,true)>]
+    [<TestCase("File hash should be the same",DestinationFileExists.True,1234L,1234L,@"c:\temp\somedestinationfile.txt","ABCDE12345ABCDE12345ABCDE12345ABCDE12345","ABCDE12345ABCDE12345ABCDE12345ABCDE12345",Expected.True,CallFileExists.True,CallGetFileSize.True,CallComputeFileHashFromHashLength.True)>]
+    [<TestCase("File hash should not be the same",DestinationFileExists.True,1234L,1234L,@"c:\temp\somedestinationfile.txt","ABCDE12345ABCDE12345ABCDE12345ABCDE12345","99CDE12345ABCDE12345ABCDE12345ABCDE12399",Expected.False,CallFileExists.True,CallGetFileSize.True,CallComputeFileHashFromHashLength.True)>]
+    [<TestCase("File hash should not be the same due file size difference.",DestinationFileExists.True,0L,1234L,@"c:\temp\somedestinationfile.txt","NOTUSED","NOTUSED",Expected.False,CallFileExists.True,CallGetFileSize.True,CallComputeFileHashFromHashLength.False)>]
+    [<TestCase("File hash should not be the same due destinaton file not exists.",DestinationFileExists.False,1234L,1234L,@"c:\temp\somedestinationfile.txt","NOTUSED","NOTUSED",Expected.False,CallFileExists.True,CallGetFileSize.False,CallComputeFileHashFromHashLength.False)>]
+    [<TestCase("File hash should be the same if source hash is empty.",DestinationFileExists.True,1234,1234,@"c:\temp\somedestinationfile.txt","","NOTUSED",Expected.True,CallFileExists.True,CallGetFileSize.True,CallComputeFileHashFromHashLength.True)>]
+    [<TestCase("File hash should be the same if source hash is empty and source size is 0.",DestinationFileExists.True,1234L,0L,@"c:\temp\somedestinationfile.txt","","NOTUSED",Expected.True,CallFileExists.True,CallGetFileSize.True,CallComputeFileHashFromHashLength.True)>]
     
     let hasSameFileHashPartialTest (testDescription:string) (destinationFileExists:Boolean) (destinationFileSize:Int64) (sourceFileSize:Int64) (destinationFilePath:string) (sourceFileHash:string) (destinatinationFileHash:string)  (expected:Boolean) (callFileExists:Boolean) (callgetFileSize:Boolean) (callComputeFileHashFromHashLength:Boolean) =
         //Setup
@@ -24,7 +53,7 @@ module ChecksumTests =
             destinationFileExists
             )
         let getFileSizeStub = (fun f ->
-            Assert.IsTrue(callFileExists,"getFileSize should not have been called")
+            Assert.IsTrue(callgetFileSize,"getFileSize should not have been called")
             destinationFileSize
             )
         
