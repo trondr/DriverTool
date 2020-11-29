7f0817dae5b5fca7ce3d2a3cc533886aa1e20db4
trondr <trondr@outlook.com>
2020-11-29 22:58:15 +0100

    Code cleanup. Remove Akka.NET from x86 host project.

--- src/app/DriverTool.x86.Host/RunHost.fs
+++ src/app/DriverTool.x86.Host/RunHost.fs
@@ -4,48 +4,10 @@ module RunHost =
     open System
     open System.ServiceModel
     open System.ServiceModel.Description
-    open DriverTool.x86.Host.HostActors
-    open Akka.FSharp
-    open DriverTool.Library.HostMessages
     open DriverTool.Library.Logging
     let logger = getLoggerByName "RunHost"
 
-    let hoconConfig port = 
-        let configString = """
-akka {  
-    loglevel = "INFO"
-    actor {
-        debug {            
-            receive = off
-            autoreceive = off
-            lifecycle = off
-            event-stream = off
-            unhandled = off
-        }        
-        provider = remote
-    }
-    remote {
-        log-sent-messages = off
-        dot-netty.tcp {
-            port = 8081
-            hostname = 0.0.0.0
-            public-hostname = localhost
-        }
-    }
-}
-"""
-        configString.Replace("8081", port) //Replace the default 8081 port with specified port.
-            
-    let runHost port =
-        let config = Akka.FSharp.Configuration.parse (hoconConfig port)
-        use system = Akka.FSharp.System.create "HostSystem" config
-        let hostActor = Akka.FSharp.Spawn.spawn system "HostActor" hostActor
-        hostActor <! "DriverTool x86 host is now listening for requests."
-        system.WhenTerminated.Wait()
-        logger.Info("DriverTool x86 host has been terminated.")
-        0
-
-    let runHost2 () =
+    let runHost () =
         let serviceUri = new Uri("http://localhost:8733/Design_Time_Addresses/DriverTool.x86.Service/ToolService/")
         let host = new ServiceHost(typeof<DriverTool.x86.Service.ToolService>, serviceUri)
         host.AddServiceEndpoint(typeof<DriverTool.x86.Service.IToolService>, new WSHttpBinding(), "") |> ignore
@@ -53,7 +15,7 @@ akka {
         smb.HttpGetEnabled <- true
         host.Description.Behaviors.Add(smb)
         host.Open()
-        Console.WriteLine("Service is host at " + serviceUri.ToString());
+        Console.WriteLine("Service is hosted at " + serviceUri.ToString());
         let cancelationTokenSource = new System.Threading.CancellationTokenSource()
         cancelationTokenSource.Token.WaitHandle.WaitOne() |>ignore 
         0
