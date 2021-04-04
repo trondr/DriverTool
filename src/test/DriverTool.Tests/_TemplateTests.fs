a2e464824c886f8ddaa046ae5f2f548b98a3485c
trondr <trondr@outlook.com>
2021-04-04 18:41:53 +0200

    Add unit test template

---
+++ src/test/DriverTool.Tests/_TemplateTests.fs
@@ -0,0 +1,23 @@
+﻿namespace DriverTool.Tests
+
+open NUnit.Framework
+
+[<TestFixture>]
+[<Category(TestCategory.UnitTests)>]
+module _TemplateTests=
+    open DriverTool.Library
+    
+    [<SetUp>]    
+    let setup () =
+        DriverTool.Library.Logging.configureConsoleLogging()
+
+    //[<Test>]
+    [<Category(TestCategory.UnitTests)>]
+    let someTest () =
+        match(result{
+            let! actual = Result.Error (toException "Not Implemented" None)
+            return actual
+        })with
+        |Result.Ok a -> Assert.IsTrue(true)
+        |Result.Error ex -> Assert.Fail(ex.Message)
+
