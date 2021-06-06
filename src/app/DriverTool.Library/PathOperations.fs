c0dab043560bf0a4f86e0d0bc0643cd9806b05a9
trondr <trondr@outlook.com>
2021-06-06 17:03:12 +0200

    Implement unc path conversions.

--- src/app/DriverTool.Library/PathOperations.fs
+++ src/app/DriverTool.Library/PathOperations.fs
@@ -24,4 +24,95 @@ module PathOperations =
 
     let getFileNameFromPath path = 
         System.IO.Path.GetFileName(FileSystem.pathValue path)
-     
+    
+    let isUncPath path =
+        let uri = new System.Uri(FileSystem.pathValue path)
+        uri.IsUnc
+
+    let getDrive path =
+        match (FileSystem.pathValue path) with
+        |Regex @"^([a-zA-Z]):.+$" [drive] -> Result.Ok drive
+        |_ -> Result.Error (toException (sprintf "Failed to get drive. Path '%s' does not contain a drive." (FileSystem.pathValue path)) None)
+
+    let getPathTail path =
+        match (FileSystem.pathValue path) with
+        |Regex @"^[a-zA-Z]:\\(.+)$" [pathTail] -> Result.Ok pathTail
+        |_ -> Result.Error (toException (sprintf "Failed to get path tail. Path '%s' does not contain a drive." (FileSystem.pathValue path)) None)
+
+    let isNetworkDrive drive =
+        let driveInfo = new System.IO.DriveInfo(drive)
+        driveInfo.DriveType = System.IO.DriveType.Network
+      
+    open System
+    open System.Runtime.InteropServices
+    open System.ComponentModel
+    [<DllImport("mpr.dll")>]
+    extern int WNetGetUniversalName(string lpLocalPath, [<MarshalAs(UnmanagedType.U4)>] int dwInfoLevel, IntPtr lpBuffer, [<MarshalAs(UnmanagedType.U4)>] int& lpBufferSize)
+
+    let networkDriveToUncPath drive =        
+        let mutable buffer = Marshal.AllocCoTaskMem(1)
+        try
+            let mutable size = 0            
+            let drivePath = sprintf "%s:\\" drive
+            let UniversalNameInfoLevel = 0x00000001
+            let ErrorMoreData = 234
+            let ErrorSuccess = 0
+            let apiReturnValue = WNetGetUniversalName (drivePath,UniversalNameInfoLevel,buffer, &size)
+            match apiReturnValue with
+            |x when x = ErrorMoreData ->
+                Marshal.FreeCoTaskMem(buffer)    
+                buffer <- Marshal.AllocCoTaskMem(size)
+                let apiReturnValue2 = WNetGetUniversalName (drivePath,UniversalNameInfoLevel,buffer, &size)
+                match apiReturnValue2 with
+                | x when x = ErrorSuccess ->
+                    let uncPath = Marshal.PtrToStringAnsi(new IntPtr(buffer.ToInt64() + Convert.ToInt64(IntPtr.Size)), size) 
+                    FileSystem.path (uncPath.Substring(0, uncPath.IndexOf('\x00')))
+                | _ ->
+                    Result.Error (toException (sprintf "Failed to get unc path from network drive '%s'." drive) (Some (new Win32Exception(apiReturnValue2) :> Exception)))
+            |_ ->                               
+                Result.Error (toException (sprintf "Failed to get unc path from network drive '%s'." drive) (Some (new Win32Exception(apiReturnValue) :> Exception)))
+        finally
+            Marshal.FreeCoTaskMem(buffer)
+    
+    ///Convert local drive to unc path using admin share
+    let localDriveToUncPath localDrive getComputerName =
+        result{
+            let path = sprintf "\\\\%s\\%s$" (getComputerName()) localDrive
+            let! uncPath = FileSystem.path path
+            return uncPath        
+        }
+
+    ///Convert path to unc path. If local drive convert to unc path using admin share if allowAdminShareForLocalDrive = true.
+    let toUncPath useAdminShareForLocalDrive path = 
+        result{
+            let! uncPath =
+                match (isUncPath path) with
+                |true -> Result.Ok path
+                |false ->
+                    result{
+                        let! drive = getDrive path
+                        let! pathTail = getPathTail path                         
+                        let! uncPath =
+                            match (isNetworkDrive drive) with
+                            |true ->
+                                //network drive
+                                result{
+                                    let! uncPathShare = networkDriveToUncPath drive
+                                    let! uncPath = combinePaths2 uncPathShare pathTail
+                                    return uncPath                        
+                                }
+                            |false ->
+                                //local drive
+                                match useAdminShareForLocalDrive with
+                                |true ->
+                                    result{
+                                        let! localUncPathShare = localDriveToUncPath drive (fun () -> System.Environment.MachineName)
+                                        let! uncPath = combinePaths2 localUncPathShare pathTail
+                                        return uncPath                            
+                                    }
+                                |false -> Result.Error (toException (sprintf "Failed to get unc path from '%s'. The path is a local path." (FileSystem.pathValue path)) None)
+                        return uncPath
+                    }
+            return uncPath
+        }
+    
---
+++ src/test/DriverTool.Tests/PathOperationsTests.fs
@@ -0,0 +1,77 @@
+﻿namespace DriverTool.Tests
+
+open NUnit.Framework
+
+[<TestFixture>]
+module PathOperationsTests=
+    open DriverTool.Library
+    
+    [<SetUp>]    
+    let setup () =
+        DriverTool.Library.Logging.configureConsoleLogging()
+
+    [<Test>]
+    [<TestCase(@"\\someserver\share",true)>]
+    [<TestCase(@"c:\some\local\path",false)>]
+    [<Category(TestCategory.UnitTests)>]
+    let isUncPathTests (path,exepected) =
+        match(result{
+            let! testPath = FileSystem.path path
+            let actual = PathOperations.isUncPath testPath
+            Assert.AreEqual(actual,exepected)
+            return actual
+        })with
+        |Result.Ok a -> Assert.IsTrue(true)
+        |Result.Error ex -> Assert.Fail(getAccumulatedExceptionMessages ex)
+
+
+    [<Test>]
+    [<TestCase(@"\\someserver\share",false,null,"Failed to get drive. Path '\\\\someserver\\share' does not contain a drive.")>]
+    [<TestCase(@"c:\some\local\path",true,"c",null)>]
+    [<Category(TestCategory.UnitTests)>]
+    let getDriveTests (path,isSuccess,exepected:string,expectedErrorMessage:string) =
+        match(result{
+            let! testPath = FileSystem.path path
+            let! actual = PathOperations.getDrive testPath
+            Assert.AreEqual(actual,exepected)
+            return actual
+        })with
+        |Result.Ok a -> Assert.IsTrue(isSuccess)
+        |Result.Error ex -> 
+            Assert.IsFalse(isSuccess)
+            Assert.AreEqual(expectedErrorMessage,ex.Message)
+
+
+    [<Test>]
+    [<TestCase("Y",@"\\teta410-cm01.teta410.local\PkgSrc$\",true,"")>]
+    [<Category(TestCategory.IntegrationTests)>]
+    let networkDriveToUncPathTests (drive,expected:string,isSuccess,expectedErrorMessage) =
+        match(result{            
+            let! actual = PathOperations.networkDriveToUncPath drive
+            Assert.AreEqual(expected,FileSystem.pathValue actual)
+            return actual
+        })with
+        |Result.Ok a -> Assert.IsTrue(isSuccess)
+        |Result.Error ex -> 
+            logger.Error(getAccumulatedExceptionMessages ex)
+            Assert.IsFalse(isSuccess)
+            Assert.AreEqual(expectedErrorMessage,ex.Message)
+
+    [<Test>]
+    [<TestCase(@"\\teta410-cm01.teta410.local\PkgSrc$\Applications",false,true,@"\\teta410-cm01.teta410.local\PkgSrc$\Applications","")>]
+    [<TestCase(@"y:\Applications",false,true,@"\\teta410-cm01.teta410.local\PkgSrc$\Applications","")>]
+    [<TestCase(@"c:\temp",false,false,null,@"Failed to get unc path from 'c:\temp'. The path is a local path.")>]
+    [<TestCase(@"c:\temp",true,true,@"\\MININT-MJ4LUGO\c$\temp","")>]
+    [<Category(TestCategory.IntegrationTests)>]
+    let toUncPathTests (path,useLocalAdminShare,isSuccess,expected:string,expectedErrorMessage:string) =
+        match(result{            
+            let! testPath = FileSystem.path path
+            let! actual = PathOperations.toUncPath useLocalAdminShare testPath            
+            Assert.AreEqual(expected,FileSystem.pathValue actual)
+            return actual
+        })with
+        |Result.Ok a -> Assert.IsTrue(isSuccess)
+        |Result.Error ex -> 
+            logger.Error(getAccumulatedExceptionMessages ex)
+            Assert.IsFalse(isSuccess)
+            Assert.AreEqual(expectedErrorMessage,ex.Message)
