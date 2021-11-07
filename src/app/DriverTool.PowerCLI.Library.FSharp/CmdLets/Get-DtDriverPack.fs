e64eb6e9f4322e6e2bdcc8c72f0fc745148c0bc6
trondr <trondr@outlook.com>
2021-11-07 23:05:49 +0100

    Get-DtDriverPack: Add support for OperatinSystem and OsBuild parameter.

--- src/app/DriverTool.PowerCLI.Library.FSharp/CmdLets/Get-DtDriverPack.fs
+++ src/app/DriverTool.PowerCLI.Library.FSharp/CmdLets/Get-DtDriverPack.fs
@@ -30,6 +30,21 @@ module M =
         |>Array.filter(fun dp -> dp.Manufacturer = manufacturer)
         |>Array.map(fun dp -> dp.ModelCodes |> Array.map(fun m -> m))
         |>Array.collect id
+
+    let getOperatingSystems manufacturer modelCode (driverPacks:DriverPack.DriverPackInfo[]) =
+        driverPacks
+        |>Array.filter(fun dp -> dp.Manufacturer = manufacturer)
+        |>Array.filter(fun dp ->  dp.ModelCodes |> Array.contains modelCode)
+        |>Array.map(fun dp-> dp.Os)
+        |>Array.distinct
+
+    let getOsBuild manufacturer modelCode operatingSystem (driverPacks:DriverPack.DriverPackInfo[]) =
+        driverPacks
+        |>Array.filter(fun dp -> dp.Manufacturer = manufacturer)
+        |>Array.filter(fun dp ->  dp.ModelCodes |> Array.contains modelCode)
+        |>Array.filter(fun dp -> dp.Os = operatingSystem)
+        |>Array.map(fun dp-> dp.OsBuild)
+        |>Array.distinct
     
     [<Literal>]
     let singleModelparameterSetName = "SingleModel"
@@ -57,6 +72,36 @@ type ModelCodeCompleter () =
             |>Seq.filter(fun mc -> (new WildcardPattern(wordToComplete + "*", WildcardOptions.IgnoreCase)).IsMatch(mc))
             |>Seq.map(fun mc -> new CompletionResult(mc))
 
+type OperatingSystemCompleter () =
+    interface IArgumentCompleter with
+        member this.CompleteArgument(commandName:string,parameterName:string,wordToComplete:string,commandAst:CommandAst,fakeBoundParameters:IDictionary) =
+            let modelCodes =
+                if(fakeBoundParameters.Contains("Manufacturer") && fakeBoundParameters.Contains("ModelCode")) then
+                    let manufacturerName = fakeBoundParameters.["Manufacturer"] :?>string
+                    let modelCode = fakeBoundParameters.["ModelCode"] :?>string
+                    M.getOperatingSystems manufacturerName modelCode M.allDriverPacks.Value        
+                else
+                    [||]            
+            modelCodes
+            |>Seq.filter(fun mc -> (new WildcardPattern(wordToComplete + "*", WildcardOptions.IgnoreCase)).IsMatch(mc))
+            |>Seq.map(fun mc -> new CompletionResult(mc))
+
+type OsBuildCompleter () =
+    interface IArgumentCompleter with
+        member this.CompleteArgument(commandName:string,parameterName:string,wordToComplete:string,commandAst:CommandAst,fakeBoundParameters:IDictionary) =
+            let modelCodes =
+                if(fakeBoundParameters.Contains("Manufacturer") && fakeBoundParameters.Contains("ModelCode")) then
+                    let manufacturerName = fakeBoundParameters.["Manufacturer"] :?>string
+                    let modelCode = fakeBoundParameters.["ModelCode"] :?>string
+                    let operatingSystem = fakeBoundParameters.["OperatingSystem"] :?>string
+                    M.getOsBuild manufacturerName modelCode operatingSystem M.allDriverPacks.Value        
+                else
+                    [||]            
+            modelCodes
+            |>Seq.filter(fun mc -> (new WildcardPattern(wordToComplete + "*", WildcardOptions.IgnoreCase)).IsMatch(mc))
+            |>Seq.map(fun mc -> new CompletionResult(mc))
+
+
 /// <summary>
 /// <para type="synopsis">Get one or more driver packs</para>
 /// <para type="description">Get one or more driver packs. An enteprise driver pack typically contains all drivers for a model in inf format and can be injected into the operating system using DISM.</para>
@@ -72,75 +117,53 @@ type ModelCodeCompleter () =
 type GetDtDriverPack () =
     inherit PSCmdlet ()
 
-    //Get mandatory run time parameter with validate set attribute
-    let getRunTimeParameter parameterName parameterSetName position validateSet (argumentCompleterType:System.Type) =
-        let attributeCollection = new System.Collections.ObjectModel.Collection<System.Attribute>()
-        let parameterAttribute = new System.Management.Automation.ParameterAttribute()
-        parameterAttribute.Mandatory <- true
-        parameterAttribute.Position <- position
-        parameterAttribute.ParameterSetName <- parameterSetName
-        attributeCollection.Add(parameterAttribute)        
-        if( Array.length validateSet > 0) then
-            let validateSetAttribute = new System.Management.Automation.ValidateSetAttribute(validateSet)
-            attributeCollection.Add(validateSetAttribute)
-            let completerAttribute = new System.Management.Automation.ArgumentCompleterAttribute(argumentCompleterType)
-            attributeCollection.Add(completerAttribute)
-            let runTimeParameter = new System.Management.Automation.RuntimeDefinedParameter(parameterName,typeof<string>,attributeCollection)
-            Some runTimeParameter
-        else
-            None
-
     /// <summary>
     /// <para type="description">Manufacturer.</para>
     /// </summary>
     [<Parameter(Mandatory=false,ParameterSetName=M.singleModelparameterSetName)>]
-    [<ArgumentCompleter(typeof<ManufacturerCompleter>)>]    
+    [<ArgumentCompleter(typeof<ManufacturerCompleter>)>]
     member val Manufacturer :string = System.String.Empty with get,set
     
+    /// <summary>
+    /// <para type="description">ModelCode.</para>
+    /// </summary>
+    [<Parameter(Mandatory=false,ParameterSetName=M.singleModelparameterSetName)>]
+    [<ArgumentCompleter(typeof<ModelCodeCompleter>)>]
     member val ModelCode :string = System.String.Empty with get,set
+
+    /// <summary>
+    /// <para type="description">OperatingSystem.</para>
+    /// </summary>
+    [<Parameter(Mandatory=false,ParameterSetName=M.singleModelparameterSetName)>]
+    [<ArgumentCompleter(typeof<OperatingSystemCompleter>)>]
+    member val OperatingSystem :string = System.String.Empty with get,set
+
+    /// <summary>
+    /// <para type="description">OsBuild.</para>
+    /// </summary>
+    [<Parameter(Mandatory=false,ParameterSetName=M.singleModelparameterSetName)>]
+    [<ArgumentCompleter(typeof<OsBuildCompleter>)>]
+    member val OsBuild :string = System.String.Empty with get,set
     
     [<Parameter(Mandatory=false,ParameterSetName=M.allModelsparameterSetName)>]
     member val All : SwitchParameter = new SwitchParameter(false) with get,set
     
-    override this.BeginProcessing() =
-        //TODO: Assign Manufacturer dynamic parameter value.
-        if(this.MyInvocation.BoundParameters.ContainsKey(nameof this.ModelCode)) then
-            this.ModelCode <- (this.MyInvocation.BoundParameters.[nameof this.ModelCode] :?> string)
-        else
-            this.ModelCode <- System.String.Empty
+    override this.BeginProcessing() =        
         ()
 
     override this.ProcessRecord() =                            
         if(this.All.IsPresent) then            
-            M.allDriverPacks.Value|>Array.map(fun dp -> this.WriteObject(dp)) |> ignore            
+            M.allDriverPacks.Value
+            |>Array.map(fun dp -> this.WriteObject(dp)) 
+            |> ignore            
         else            
             M.allDriverPacks.Value
             |>Array.filter(fun dp -> dp.Manufacturer = this.Manufacturer)
-            |>Array.filter(fun dp -> dp.ModelCodes|>Array.contains this.ModelCode)
-            |>Array.map(fun dp -> this.WriteObject(dp)) |> ignore
-            
-    interface IDynamicParameters with
-        member this.GetDynamicParameters() =                                    
-            let runtimeParameterDictionary = new System.Management.Automation.RuntimeDefinedParameterDictionary()            
-            
-            if(this.All.IsPresent) then
-                ()
-            else     
-                //Add ModelCode parameter
-                if (not (System.String.IsNullOrWhiteSpace(this.Manufacturer))) then
-                    let modelCodeParameterName = nameof this.ModelCode
-                    let modelCodeRuntimeParameter = getRunTimeParameter modelCodeParameterName M.singleModelparameterSetName 2 (M.getModelCodes this.Manufacturer M.allDriverPacks.Value) (typeof<ModelCodeCompleter>)
-                    match modelCodeRuntimeParameter with
-                    |Some m ->                
-                        runtimeParameterDictionary.Add(modelCodeParameterName,m)
-                    |None -> ()
-                else
-                    ()
-            
-            //TODO: Add OS parameter if Model has been defined.
-            //TODO: Add OSBuild parameter if OS has been defined.
-            runtimeParameterDictionary :> obj
-
-    
+            |>Array.filter(fun dp -> System.String.IsNullOrWhiteSpace(this.ModelCode) || dp.ModelCodes|>Array.contains this.ModelCode)
+            |>Array.filter(fun dp -> System.String.IsNullOrWhiteSpace(this.OperatingSystem) || dp.Os = this.OperatingSystem)
+            |>Array.filter(fun dp -> System.String.IsNullOrWhiteSpace(this.OsBuild) || dp.OsBuild = this.OsBuild)
+            |>Array.map(fun dp -> this.WriteObject(dp))
+            |> ignore
+  
 
     
