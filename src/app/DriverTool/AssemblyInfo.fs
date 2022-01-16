a5c961b32baf694cc671a19c8d8ea7e02e03d875
trondr <trondr@outlook.com>
2022-01-16 20:38:43 +0100

    Build version 22016

--- src/app/DriverTool/AssemblyInfo.fs
+++ src/app/DriverTool/AssemblyInfo.fs
@@ -8,9 +8,9 @@ open System.Runtime.InteropServices
 [<assembly: AssemblyDescriptionAttribute("Download drivers and software for current PC model and create a driver package that can be imported into SCCM as a package or application.")>]
 [<assembly: AssemblyProductAttribute("DriverTool")>]
 [<assembly: AssemblyCompanyAttribute("github/trondr")>]
-[<assembly: AssemblyCopyrightAttribute("Copyright © github/trondr 2018-2021")>]
-[<assembly: AssemblyVersionAttribute("1.0.21290.62")>]
-[<assembly: AssemblyFileVersionAttribute("1.0.21290.62")>]
+[<assembly: AssemblyCopyrightAttribute("Copyright © github/trondr 2018-2022")>]
+[<assembly: AssemblyVersionAttribute("1.0.22016.62")>]
+[<assembly: AssemblyFileVersionAttribute("1.0.22016.62")>]
 [<assembly: ComVisibleAttribute(false)>]
 [<assembly: GuidAttribute("19822aea-c088-455d-b5a5-4738a3a9dba7")>]
 [<assembly: InternalsVisibleToAttribute("DriverTool.Tests")>]
@@ -21,9 +21,9 @@ module internal AssemblyVersionInformation =
     let [<Literal>] AssemblyDescription = "Download drivers and software for current PC model and create a driver package that can be imported into SCCM as a package or application."
     let [<Literal>] AssemblyProduct = "DriverTool"
     let [<Literal>] AssemblyCompany = "github/trondr"
-    let [<Literal>] AssemblyCopyright = "Copyright © github/trondr 2018-2021"
-    let [<Literal>] AssemblyVersion = "1.0.21290.62"
-    let [<Literal>] AssemblyFileVersion = "1.0.21290.62"
+    let [<Literal>] AssemblyCopyright = "Copyright © github/trondr 2018-2022"
+    let [<Literal>] AssemblyVersion = "1.0.22016.62"
+    let [<Literal>] AssemblyFileVersion = "1.0.22016.62"
     let [<Literal>] ComVisible = false
     let [<Literal>] Guid = "19822aea-c088-455d-b5a5-4738a3a9dba7"
     let [<Literal>] InternalsVisibleTo = "DriverTool.Tests"
